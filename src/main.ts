242f28c97cac41b720404708fa72f3ce99510b4d
Manfred Steyer <manfred.steyer@gmx.net>
2022-12-07 12:45:35 +0100

    install husky + pretty-quick

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: []
+  providers: [],
 });
