9bf651d909f8c3f718961cd0b52b240fc14b9d5d
Manfred Steyer <manfred.steyer@gmx.net>
2022-12-07 13:06:23 +0100

    add simple template

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [],
+  providers: [provideHttpClient()],
 });
