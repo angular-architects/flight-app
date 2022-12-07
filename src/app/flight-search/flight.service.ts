208d6deac53ab80122b67a1b853ffe9d41183d80
Manfred Steyer <manfred.steyer@gmx.net>
2022-12-07 21:45:17 +0100

    add classic provider

--- src/app/flight-search/flight.service.ts
+++ src/app/flight-search/flight.service.ts
@@ -1,21 +1,8 @@
-import { inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Flight } from '../model/flight';
-import { DefaultFlightService } from './default-flight.service';
-import { DummyFlightService } from './dummy-flight.service';
 
-const DEBUG = false;
-
-@Injectable({
-  providedIn: 'root',
-  useFactory: () => {
-    if (DEBUG) {
-      return inject(DummyFlightService);
-    } else {
-      return inject(DefaultFlightService);
-    }
-  },
-})
+@Injectable()
 export abstract class FlightService {
   abstract find(from: string, to: string): Observable<Flight[]>;
 }
