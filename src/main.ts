002a4e3853b2430b67c48e09a202abcd0986409b
Manfred Steyer <manfred.steyer@gmx.net>
2022-12-08 13:59:38 +0100

    add material dialog

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { NextFlightsModule } from './app/next-flights/next-flights.module';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient(), importProvidersFrom(NextFlightsModule)],
+  providers: [
+    provideHttpClient(),
+    importProvidersFrom(NextFlightsModule),
+    importProvidersFrom(MatDialogModule),
+  ],
 });
