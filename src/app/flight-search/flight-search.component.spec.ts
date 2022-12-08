7d07bbd4315995fdb755aab0fd3772298abf1209
Manfred Steyer <manfred.steyer@gmx.net>
2022-12-08 21:49:01 +0100

    add first simple test

--- src/app/flight-search/flight-search.component.spec.ts
+++ src/app/flight-search/flight-search.component.spec.ts
@@ -1,22 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { FlightSearchComponent } from './flight-search.component';
 
-describe('FlightSearchComponent', () => {
+describe('Unit test: flight-search.component', () => {
   let component: FlightSearchComponent;
   let fixture: ComponentFixture<FlightSearchComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       imports: [FlightSearchComponent],
-    }).compileComponents();
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
 
     fixture = TestBed.createComponent(FlightSearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should not have any flights loaded initially', () => {
+    expect(component.flights.length).toBe(0);
   });
 });
--- src/app/shared/validation/async-city-validator.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AsyncCityValidatorDirective } from './async-city-validator.directive';
-
-describe('AsyncCityValidatorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AsyncCityValidatorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
