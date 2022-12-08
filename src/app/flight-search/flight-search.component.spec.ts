29e9169832fd5c2e3297379eff96417ed549ee11
Manfred Steyer <manfred.steyer@gmx.net>
2022-12-08 21:49:01 +0100

    add first simple test

--- src/app/flight-search/flight-search.component.spec.ts
+++ src/app/flight-search/flight-search.component.spec.ts
@@ -1,14 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { FlightSearchComponent } from './flight-search.component';
 
-describe('FlightSearchComponent', () => {
+describe('Unit test: flight-search.component', () => {
   let component: FlightSearchComponent;
   let fixture: ComponentFixture<FlightSearchComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [FlightSearchComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(FlightSearchComponent);
@@ -16,7 +19,7 @@ describe('FlightSearchComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should not have any flights loaded initially', () => {
+    expect(component.flights.length).toBe(0);
   });
 });
--- src/app/shared/validation/roundtrip-validator.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RoundtripValidatorDirective } from './roundtrip-validator.directive';
-
-describe('RoundtripValidatorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new RoundtripValidatorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
