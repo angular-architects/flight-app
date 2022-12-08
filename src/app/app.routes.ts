4b00b6ca78e88550d90ce7176e2d18224bd320ce
Manfred Steyer <manfred.steyer@gmx.net>
2022-12-08 20:29:35 +0100

    add resolver ensuring config is loaded first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
+import { inject } from '@angular/core';
 import { Routes } from '@angular/router';
+import { delay, filter } from 'rxjs';
 import { AboutComponent } from './about/about.component';
 import { FLIGHT_BOOKING_ROUTES } from './flight-booking/flight-booking.routes';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ConfigService } from './shared/config.service';
 
 export const APP_ROUTES: Routes = [
-  ...FLIGHT_BOOKING_ROUTES,
   {
     path: '',
     pathMatch: 'full',
@@ -15,13 +17,24 @@ export const APP_ROUTES: Routes = [
     path: 'home',
     component: HomeComponent,
   },
+
   {
-    path: 'about',
-    component: AboutComponent,
-  },
-  // This _needs_ to be the last route!!
-  {
-    path: '**',
-    component: NotFoundComponent,
+    path: '',
+    resolve: {
+      config: () => inject(ConfigService).loaded$,
+    },
+    children: [
+      ...FLIGHT_BOOKING_ROUTES,
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+
+      // This _needs_ to be the last route!!
+      {
+        path: '**',
+        component: NotFoundComponent,
+      },
+    ],
   },
 ];
