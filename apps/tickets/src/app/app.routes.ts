37df790c55fcfe082bb18ee24d5fead9c435cd8e
Manfred Steyer <manfred.steyer@gmx.net>
2022-12-12 00:51:53 +0100

    reference multiple micro frontends

--- apps/tickets/src/app/app.routes.ts
+++ apps/tickets/src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { loadRemoteModule } from '@angular-architects/module-federation';
+import {
+  startsWith,
+  WebComponentWrapper,
+  WebComponentWrapperOptions,
+} from '@angular-architects/module-federation-tools';
 import { inject } from '@angular/core';
 import { Routes } from '@angular/router';
 import { ConfigService } from '@flight-demo/shared/util-config';
@@ -25,6 +30,45 @@ export const APP_ROUTES: Routes = [
   {
     path: 'luggage',
     loadChildren: () => loadRemoteModule('luggage', './routes'),
+  },
+
+  {
+    path: 'angular2',
+    component: WebComponentWrapper,
+    data: {
+      type: 'script',
+      remoteEntry:
+        'https://gray-pond-030798810.azurestaticapps.net/remoteEntry.js',
+      remoteName: 'angular2',
+      exposedModule: './web-components',
+      elementName: 'angular2-element',
+    } as WebComponentWrapperOptions,
+  },
+
+  {
+    path: 'react',
+    component: WebComponentWrapper,
+    data: {
+      type: 'script',
+      remoteEntry:
+        'https://witty-wave-0a695f710.azurestaticapps.net/remoteEntry.js',
+      remoteName: 'react',
+      exposedModule: './web-components',
+      elementName: 'react-element',
+    } as WebComponentWrapperOptions,
+  },
+
+  {
+    matcher: startsWith('angular3'),
+    component: WebComponentWrapper,
+    data: {
+      type: 'script',
+      remoteEntry:
+        'https://gray-river-0b8c23a10.azurestaticapps.net/remoteEntry.js',
+      remoteName: 'angular3',
+      exposedModule: './web-components',
+      elementName: 'angular3-element',
+    } as WebComponentWrapperOptions,
   },
 
   {
