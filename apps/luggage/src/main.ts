735a78fe0a9db47d69fb40811bfa8aea9d85605d
Manfred Steyer <manfred.steyer@gmx.net>
2022-12-11 21:31:05 +0100

    violate boundaries

--- apps/luggage/src/main.ts
+++ apps/luggage/src/main.ts
@@ -1,14 +1,8 @@
-import { HttpClientModule } from '@angular/common/http';
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(HttpClientModule)],
+  providers: [provideHttpClient()],
 });
