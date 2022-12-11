c97675ae7768cadf135a20449220ecac2cc28dba
Manfred Steyer <manfred.steyer@gmx.net>
2022-12-12 00:07:35 +0100

    load component directly

--- libs/tickets/feature-next-flights/src/lib/next-flights.component.ts
+++ libs/tickets/feature-next-flights/src/lib/next-flights.component.ts
@@ -1,12 +1,41 @@
-import { Component, inject } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  inject,
+  Injector,
+  Type,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import { NextFlightsService } from './next-flights.service';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
 @Component({
   selector: 'app-next-flights',
   templateUrl: './next-flights.component.html',
   styleUrls: ['./next-flights.component.css'],
 })
-export class NextFlightsComponent {
+export class NextFlightsComponent implements AfterViewInit {
+  @ViewChild('placeholder', { read: ViewContainerRef })
+  placeholder: ViewContainerRef | undefined;
+
+  injector = inject(Injector);
   nextFlightsService = inject(NextFlightsService);
   flights$ = this.nextFlightsService.load();
+
+  comp: Type<unknown> | undefined;
+
+  async ngAfterViewInit() {
+    if (!this.placeholder) return;
+
+    const m = await loadRemoteModule('checkin', './Component');
+    this.placeholder.createComponent(m.AppComponent, {
+      injector: this.injector,
+    });
+
+    // Working with properties and events:
+    // const comp = this.placeholder.createComponent(m.AppComponent, { injector: this.injector });
+    // comp.setInput('myProp', 'value');
+    // comp.instance['event'].subscribe(...)
+  }
 }
