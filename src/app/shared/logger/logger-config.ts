2cbbfd2154bd14085c0b241fe6ef4a3cf96eac2b
Manfred Steyer <manfred.steyer@gmx.net>
2023-02-03 16:00:18 +0100

    cleanup logger config

--- src/app/shared/logger/logger-config.ts
+++ src/app/shared/logger/logger-config.ts
@@ -1,15 +1,9 @@
-import { Type } from '@angular/core';
-import { DefaultLogFormatter, LogFormatter } from './log-formatter';
 import { LogLevel } from './log-level';
 
 export abstract class LoggerConfig {
   abstract level: LogLevel;
-  abstract chaining: boolean;
-  abstract formatter: Type<LogFormatter>;
 }
 
 export const defaultConfig: LoggerConfig = {
   level: LogLevel.DEBUG,
-  chaining: false,
-  formatter: DefaultLogFormatter,
 };
