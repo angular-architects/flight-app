6affe7be5881d85ecd5a038c44189b0fbe8258d0
Manfred Steyer <manfred.steyer@gmx.net>
2023-03-13 14:46:21 +0100

    add generic loading indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
+import { AsyncPipe, NgIf } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+  RouterOutlet,
+} from '@angular/router';
+import { filter, map, merge, Observable } from 'rxjs';
 import { FlightSearchComponent } from './flight-booking/flight-search/flight-search.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NextFlightsModule } from './next-flights/next-flights.module';
@@ -14,6 +23,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     FlightSearchComponent,
     NextFlightsModule,
     RouterOutlet,
+    AsyncPipe,
+    NgIf,
   ],
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -21,12 +32,30 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 })
 export class AppComponent {
   title = 'Hello World!';
-
   configService = inject(ConfigService);
+  router = inject(Router);
+  loading$: Observable<boolean>;
 
   constructor() {
     // TODO: In a later lab, we will assure that
     //  loading did happen _before_ we use the config!
     this.configService.loadConfig();
+
+    const stop$ = this.router.events.pipe(
+      filter(
+        (e) =>
+          e instanceof NavigationEnd ||
+          e instanceof NavigationError ||
+          e instanceof NavigationCancel
+      ),
+      map(() => false)
+    );
+
+    const start$ = this.router.events.pipe(
+      filter((e) => e instanceof NavigationStart),
+      map(() => true)
+    );
+
+    this.loading$ = merge(start$, stop$);
   }
 }
