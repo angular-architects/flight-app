8cfb382d49f1747828e114ba0bc33fa9fe6e5fc8
Manfred Steyer <manfred.steyer@gmx.net>
2023-03-13 13:22:44 +0100

    add basket

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,25 +1,34 @@
-import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Renderer2,
+} from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import {
+  Location,
+  LocationStrategy,
+  PathLocationStrategy,
+} from '@angular/common';
 
 @Component({
-    standalone: true,
-    selector: 'app-navbar-cmp',
-    templateUrl: 'navbar.component.html'
+  standalone: true,
+  selector: 'app-navbar-cmp',
+  templateUrl: 'navbar.component.html',
 })
 export class NavbarComponent {
-    
-    sidebarVisible: boolean = false;
+  sidebarVisible = false;
 
-    sidebarToggle(){
-        var body = document.getElementsByTagName('body')[0];
+  sidebarToggle() {
+    const body = document.getElementsByTagName('body')[0];
 
-        if(this.sidebarVisible == false){
-            body.classList.add('nav-open');
-            this.sidebarVisible = true;
-        } else {
-            this.sidebarVisible = false;
-            body.classList.remove('nav-open');
-        }
+    if (this.sidebarVisible == false) {
+      body.classList.add('nav-open');
+      this.sidebarVisible = true;
+    } else {
+      this.sidebarVisible = false;
+      body.classList.remove('nav-open');
     }
+  }
 }
