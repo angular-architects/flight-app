4f54599b58f22eedcefcf545519ff0d74eb23238
Manfred Steyer <manfred.steyer@gmx.net>
2023-04-17 21:39:58 +0200

    improvde dummy flight service

--- src/app/flight-booking/flight-search/dummy-flight.service.ts
+++ src/app/flight-booking/flight-search/dummy-flight.service.ts
@@ -9,9 +9,18 @@ export class DummyFlightService implements FlightService {
     const date = new Date().toISOString();
 
     return of([
-      { id: 7, from: 'here', to: 'there', date, delayed: false },
-      { id: 8, from: 'here', to: 'there', date, delayed: false },
-      { id: 9, from: 'here', to: 'there', date, delayed: false },
+      { id: 7, from: 'Graz', to: 'Hamburg', date, delayed: false },
+      { id: 8, from: 'Graz', to: 'Hamburg', date, delayed: false },
+      { id: 9, from: 'Hamburg', to: 'Graz', date, delayed: false },
+      { id: 10, from: 'Hamburg', to: 'Graz', date, delayed: false },
+      { id: 11, from: 'London', to: 'Paris', date, delayed: false },
+      { id: 12, from: 'London', to: 'Paris', date, delayed: false },
+      { id: 13, from: 'Paris', to: 'London', date, delayed: false },
+      { id: 14, from: 'Paris', to: 'London', date, delayed: false },
     ]);
+  }
+
+  findById(id: string): Observable<Flight> {
+    throw new Error('Method not implemented.');
   }
 }
