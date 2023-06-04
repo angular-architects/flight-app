6dc1d574c2d7a24a649bb8b72ff6ab21510fce8f
Manfred Steyer <manfred.steyer@gmx.net>
2023-06-04 22:12:01 +0200

    chore: cleanup for labs

--- src/app/domains/ticketing/data/+state/actions.ts
+++ src/app/domains/ticketing/data/+state/actions.ts
@@ -4,13 +4,8 @@ import { Flight } from '../flight';
 export const ticketingActions = createActionGroup({
   source: 'ticketing',
   events: {
-    // event, command, document
     'flights loaded': props<{ flights: Flight[] }>(),
-    // event
-
     'update flight': props<{ flight: Flight }>(),
-    // command
-
     'clear flights': emptyProps(),
   },
 });
