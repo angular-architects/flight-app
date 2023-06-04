9f0a4ac44c3913d541c6c59c87bc1f175496ff8c
Manfred Steyer <manfred.steyer@gmx.net>
2023-06-04 21:29:18 +0200

    use rxjs interop for debouncing

--- src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
+++ src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
@@ -1,14 +1,10 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  Injector,
-  OnInit,
   computed,
   effect,
   inject,
-  runInInjectionContext,
   signal,
-  untracked,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -16,6 +12,8 @@ import { FlightCardComponent } from '../flight-card/flight-card.component';
 import { CityPipe } from '@demo/shared/ui-common';
 import { Flight, FlightService } from '@demo/ticketing/data';
 import { addMinutes } from '@demo/shared/util-common';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { combineLatest, debounceTime, filter } from 'rxjs';
 
 @Component({
   selector: 'app-flight-search',
@@ -25,98 +23,53 @@ import { addMinutes } from '@demo/shared/util-common';
   imports: [CommonModule, FormsModule, CityPipe, FlightCardComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FlightSearchComponent implements OnInit {
+export class FlightSearchComponent {
+  private flightService = inject(FlightService);
+
   from = signal('Paris');
   to = signal('London');
 
-  flightRoute = computed(() => this.from() + ' to ' + this.to());
-
   flights = signal<Array<Flight>>([]);
-  selectedFlight = signal<Flight | undefined>(undefined);
-
-  message = signal('');
-
-  date = signal(new Date());
-
   basket = signal<Record<number, boolean>>({
     3: true,
     5: true,
   });
 
-  injector = inject(Injector);
+  from$ = toObservable(this.from);
+  to$ = toObservable(this.to);
 
-  private flightService = inject(FlightService);
+  criteria$ = combineLatest({ from: this.from$, to: this.to$ }).pipe(
+    filter((combi) => combi.from.length >= 3 && combi.to.length >= 3),
+    debounceTime(300)
+  );
+  criteria = toSignal(this.criteria$, {
+    initialValue: {
+      from: '',
+      to: '',
+    },
+  });
+
+  flightRoute = computed(() => this.from() + ' to ' + this.to());
 
   constructor() {
-    effect(() => {
-      console.log('from', this.from());
-      console.log(
-        'to',
-        untracked(() => this.to())
-      );
-    });
-
-    setTimeout(() => {
-      this.from.set('Graz');
-      // Graz - London  // Glitch
-      this.to.set('Hamburg');
-      // Graz - Hamburg
-    }, 2000);
-
     effect(
       () => {
-        // console.log('from', this.from());
-        // console.log('to', untracked(() => this.to()));
         this.search();
       },
       { allowSignalWrites: true }
     );
-
-    // effect(() => {
-    //   this.to.set(this.from())
-    // }, { allowSignalWrites: true });
   }
 
-  ngOnInit() {
-    effect(
-      () => {
-        console.log('from', this.from());
-        console.log('to', () => this.to());
-      },
-      { injector: this.injector }
-    );
-  }
-
-  search(): void {
-    // Reset properties
-    this.message.set('');
-    this.selectedFlight.set(undefined);
-
-    this.flightService.find(this.from(), this.to()).subscribe({
-      next: (flights) => {
-        this.flights.set(flights);
-      },
-      error: (errResp) => {
-        console.error('Error loading flights', errResp);
-      },
-    });
-  }
-
-  select(f: Flight): void {
-    this.selectedFlight.set(f);
+  async search(): Promise<void> {
+    const { from, to } = this.criteria();
+    if (!from || !to) {
+      return;
+    }
+    const flights = await this.flightService.findPromise(from, to);
+    this.flights.set(flights);
   }
 
   updateBasket(fid: number, selected: boolean): void {
-    // const basket = this.basket();
-    // basket[fid] = selected;
-    // this.basket.set(basket);
-
-    // this.basket.mutate(basket => {
-    //   basket[fid] = selected;
-    // });
-
-    console.log('basket', fid, selected);
-    // Immutables
     this.basket.update((basket) => ({
       ...basket,
       [fid]: selected,
@@ -124,10 +77,6 @@ export class FlightSearchComponent implements OnInit {
   }
 
   delay(): void {
-    // this.flights.mutate(flights => {
-    //   flights[0].date = addMinutes(flights[0].date, 15);
-    // });
-
     const date = addMinutes(this.flights()[0].date, 15);
 
     this.flights.update((flights) => [
