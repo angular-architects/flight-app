d6db54844ee664efb734ff13febcff3f2fb35ad1
Manfred Steyer <manfred.steyer@gmx.net>
2023-06-04 20:49:57 +0200

    chore: cleanup code wrt linting rules

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import { initFederation } from '@angular-architects/module-federation';
 
 initFederation('/assets/mf.manifest.json')
   .catch((err) => console.error(err))
-  .then((_) => import('./bootstrap'))
+  .then(() => import('./bootstrap'))
   .catch((err) => console.error(err));
