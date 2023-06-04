4a2b9fa31916427a074ee3258faf45679fc0b7fe
Manfred Steyer <manfred.steyer@gmx.net>
2023-06-04 21:05:40 +0200

    chore: cleanup for lab

--- src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
+++ src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
@@ -1,14 +1,10 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  Injector,
-  OnInit,
   computed,
   effect,
   inject,
-  runInInjectionContext,
   signal,
-  untracked,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -25,98 +21,38 @@ import { addMinutes } from '@demo/shared/util-common';
   imports: [CommonModule, FormsModule, CityPipe, FlightCardComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FlightSearchComponent implements OnInit {
+export class FlightSearchComponent {
+  private flightService = inject(FlightService);
+
   from = signal('Paris');
   to = signal('London');
 
-  flightRoute = computed(() => this.from() + ' to ' + this.to());
-
   flights = signal<Array<Flight>>([]);
-  selectedFlight = signal<Flight | undefined>(undefined);
-
-  message = signal('');
-
-  date = signal(new Date());
-
   basket = signal<Record<number, boolean>>({
     3: true,
     5: true,
   });
 
-  injector = inject(Injector);
-
-  private flightService = inject(FlightService);
+  flightRoute = computed(() => this.from() + ' to ' + this.to());
 
   constructor() {
-    effect(() => {
-      console.log('from', this.from());
-      console.log(
-        'to',
-        untracked(() => this.to())
-      );
-    });
-
-    setTimeout(() => {
-      this.from.set('Graz');
-      // Graz - London  // Glitch
-      this.to.set('Hamburg');
-      // Graz - Hamburg
-    }, 2000);
-
     effect(
       () => {
-        // console.log('from', this.from());
-        // console.log('to', untracked(() => this.to()));
         this.search();
       },
       { allowSignalWrites: true }
     );
-
-    // effect(() => {
-    //   this.to.set(this.from())
-    // }, { allowSignalWrites: true });
   }
 
-  ngOnInit() {
-    effect(
-      () => {
-        console.log('from', this.from());
-        console.log('to', () => this.to());
-      },
-      { injector: this.injector }
+  async search(): Promise<void> {
+    const flights = await this.flightService.findPromise(
+      this.from(),
+      this.to()
     );
-  }
-
-  search(): void {
-    // Reset properties
-    this.message.set('');
-    this.selectedFlight.set(undefined);
-
-    this.flightService.find(this.from(), this.to()).subscribe({
-      next: (flights) => {
-        this.flights.set(flights);
-      },
-      error: (errResp) => {
-        console.error('Error loading flights', errResp);
-      },
-    });
-  }
-
-  select(f: Flight): void {
-    this.selectedFlight.set(f);
+    this.flights.set(flights);
   }
 
   updateBasket(fid: number, selected: boolean): void {
-    // const basket = this.basket();
-    // basket[fid] = selected;
-    // this.basket.set(basket);
-
-    // this.basket.mutate(basket => {
-    //   basket[fid] = selected;
-    // });
-
-    console.log('basket', fid, selected);
-    // Immutables
     this.basket.update((basket) => ({
       ...basket,
       [fid]: selected,
@@ -124,10 +60,6 @@ export class FlightSearchComponent implements OnInit {
   }
 
   delay(): void {
-    // this.flights.mutate(flights => {
-    //   flights[0].date = addMinutes(flights[0].date, 15);
-    // });
-
     const date = addMinutes(this.flights()[0].date, 15);
 
     this.flights.update((flights) => [
