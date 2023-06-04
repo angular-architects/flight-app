225c34feba11f1a5dfea03e0b5aff1bef5b79e06
Manfred Steyer <manfred.steyer@gmx.net>
2023-06-04 21:53:26 +0200

    add facade

--- src/app/domains/ticketing/data/flight-booking.facade.ts
+++ src/app/domains/ticketing/data/flight-booking.facade.ts
@@ -1,25 +1,92 @@
-import { Injectable, inject, signal } from '@angular/core';
+import { Injectable, computed, inject, signal } from '@angular/core';
 import { FlightService } from './flight.service';
 import { Flight } from './flight';
 import { addMinutes } from '@demo/shared/util-common';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { combineLatest, debounceTime, filter } from 'rxjs';
+
+function equal(a: unknown, b: unknown): boolean {
+  return a === b;
+}
 
 @Injectable({ providedIn: 'root' })
 export class FlightBookingFacade {
   private flightService = inject(FlightService);
-  private _flights = signal<Flight[]>([]);
-  readonly flights = this._flights.asReadonly();
 
-  load(from: string, to: string): void {
-    this.flightService.find(from, to).subscribe((flights) => {
-      this._flights.set(flights);
-    });
+  private state = signal(
+    {
+      from: 'Paris',
+      to: 'London',
+      flights: [] as Flight[],
+      basket: {} as Record<number, boolean>,
+    },
+    { equal }
+  );
+
+  readonly from = computed(() => this.state().from, { equal });
+  readonly to = computed(() => this.state().to, { equal });
+  readonly flights = computed(() => this.state().flights, { equal });
+  readonly basket = computed(() => this.state().basket, { equal });
+
+  readonly flightRoute = computed(() => this.from() + ' to ' + this.to(), {
+    equal,
+  });
+
+  private from$ = toObservable(this.from);
+  private to$ = toObservable(this.to);
+
+  private criteria$ = combineLatest({
+    from: this.from$,
+    to: this.to$,
+  }).pipe(
+    filter((combi) => combi.from.length >= 3 && combi.to.length >= 3),
+    debounceTime(300)
+  );
+
+  private criteria = toSignal(this.criteria$, {
+    initialValue: {
+      from: '',
+      to: '',
+    },
+  });
+
+  async load(): Promise<void> {
+    const { from, to } = this.criteria();
+
+    if (!from || !to) {
+      return;
+    }
+
+    const flights = await this.flightService.findPromise(from, to);
+    this.state.update((state) => ({
+      ...state,
+      flights,
+    }));
+  }
+
+  upateCriteria(from: string, to: string): void {
+    this.state.update((state) => ({
+      ...state,
+      from,
+      to,
+    }));
+  }
+
+  updateBasket(fid: number, selected: boolean): void {
+    this.state.update((state) => ({
+      ...state,
+      basket: {
+        ...state.basket,
+        [fid]: selected,
+      },
+    }));
   }
 
   delay() {
     const date = addMinutes(this.flights()[0].date, 15);
-    this._flights.update((flights) => [
-      { ...flights[0], date },
-      ...flights.slice(1),
-    ]);
+    this.state.update((state) => ({
+      ...state,
+      flights: [{ ...state.flights[0], date }, ...state.flights.slice(1)],
+    }));
   }
 }
--- src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
+++ src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
@@ -1,30 +1,14 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  Injector,
-  OnInit,
-  computed,
   effect,
   inject,
-  signal,
-  untracked,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FlightCardComponent } from '../flight-card/flight-card.component';
 import { CityPipe } from '@demo/shared/ui-common';
-import {
-  Flight,
-  FlightBookingFacade,
-  FlightService,
-} from '@demo/ticketing/data';
-import { addMinutes } from '@demo/shared/util-common';
-import {
-  toSignal,
-  toObservable,
-  takeUntilDestroyed,
-} from '@angular/core/rxjs-interop';
-import { combineLatest, debounceTime, filter, interval, startWith } from 'rxjs';
+import { FlightBookingFacade } from '@demo/ticketing/data';
 
 @Component({
   selector: 'app-flight-search',
@@ -34,139 +18,23 @@ import { combineLatest, debounceTime, filter, interval, startWith } from 'rxjs';
   imports: [CommonModule, FormsModule, CityPipe, FlightCardComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FlightSearchComponent implements OnInit {
-  private facade = inject(FlightBookingFacade);
+export class FlightSearchComponent {
+  facade = inject(FlightBookingFacade);
 
-  from = signal('Paris');
-  to = signal('London');
-
-  from$ = toObservable(this.from);
-  to$ = toObservable(this.to);
-
-  criteria$ = combineLatest({
-    from: this.from$,
-    to: this.to$,
-    counter: interval(1000),
-  }).pipe(
-    filter((combi) => combi.from.length >= 3 && combi.to.length >= 3),
-    debounceTime(300)
-    // startWith({ from: '', to: ''} as {from:string, to:string})
-  );
-
-  criteria = toSignal(this.criteria$, {
-    initialValue: {
-      from: '',
-      to: '',
-      counter: -1,
-    },
-  });
-
-  // criteria2 = toSignal(this.criteria$, {
-  //   requireSync: true
-  // });
-
-  flightRoute = computed(() => this.from() + ' to ' + this.to());
+  from = this.facade.from;
+  to = this.facade.to;
 
   flights = this.facade.flights;
-  selectedFlight = signal<Flight | undefined>(undefined);
+  basket = this.facade.basket;
 
-  message = signal('');
-
-  date = signal(new Date());
-
-  basket = signal<Record<number, boolean>>({
-    3: true,
-    5: true,
-  });
-
-  injector = inject(Injector);
-
-  private flightService = inject(FlightService);
+  flightRoute = this.facade.flightRoute;
 
   constructor() {
-    this.criteria$.pipe(takeUntilDestroyed()).subscribe((c) => {
-      console.log('counter$', c.counter);
-    });
-
-    effect(() => {
-      console.log('counter', this.criteria().counter);
-    });
-
-    effect(() => {
-      console.log('from', this.from());
-      console.log(
-        'to',
-        untracked(() => this.to())
-      );
-    });
-
-    setTimeout(() => {
-      this.from.set('Graz');
-      // Graz - London  // Glitch
-      this.to.set('Hamburg');
-      // Graz - Hamburg
-    }, 2000);
-
     effect(
       () => {
-        // console.log('from', this.from());
-        // console.log('to', untracked(() => this.to()));
-        this.search();
+        this.facade.load();
       },
       { allowSignalWrites: true }
     );
-
-    // effect(() => {
-    //   this.to.set(this.from())
-    // }, { allowSignalWrites: true });
-  }
-
-  ngOnInit() {
-    effect(
-      () => {
-        console.log('from', this.from());
-        console.log('to', () => this.to());
-      },
-      { injector: this.injector }
-    );
-  }
-
-  search(): void {
-    const { from, to } = this.criteria();
-
-    // Reset properties
-    this.message.set('');
-    this.selectedFlight.set(undefined);
-
-    this.facade.load(from, to);
-  }
-
-  select(f: Flight): void {
-    this.selectedFlight.set(f);
-  }
-
-  updateBasket(fid: number, selected: boolean): void {
-    // const basket = this.basket();
-    // basket[fid] = selected;
-    // this.basket.set(basket);
-
-    // this.basket.mutate(basket => {
-    //   basket[fid] = selected;
-    // });
-
-    console.log('basket', fid, selected);
-    // Immutables
-    this.basket.update((basket) => ({
-      ...basket,
-      [fid]: selected,
-    }));
-  }
-
-  delay(): void {
-    // this.flights.mutate(flights => {
-    //   flights[0].date = addMinutes(flights[0].date, 15);
-    // });
-
-    this.facade.delay();
   }
 }
