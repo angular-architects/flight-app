ed684d7c653733c867e7d0514e2abfdc88926a33
Manfred Steyer <manfred.steyer@gmx.net>
2023-06-04 20:52:57 +0200

    chore: cleanup code wrt linting rules

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -2,11 +2,7 @@ import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { bootstrapApplication } from '@angular/platform-browser';
-import {
-  PreloadAllModules,
-  provideRouter,
-  withPreloading,
-} from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { APP_ROUTES } from './app/app.routes';
 import { NextFlightsModule } from '@demo/ticketing/feature-next-flights';
