cdcb1e3d381fc75c574a1d7c79ea393530ceaf58
Manfred Steyer <manfred.steyer@gmx.net>
2023-06-05 11:26:51 +0200

    fix: remove solution from starterkit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
-import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()],
+  providers: [],
 });
