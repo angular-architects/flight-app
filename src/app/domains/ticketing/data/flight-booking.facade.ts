b03b017a3c9ac87e2948b69afe1eccba3c1e813f
Manfred Steyer <manfred.steyer@gmx.net>
2023-06-11 12:58:42 +0200

    cleanup

--- src/app/domains/ticketing/data/flight-booking.facade.ts
+++ src/app/domains/ticketing/data/flight-booking.facade.ts
@@ -4,11 +4,6 @@ import { ticketingActions } from './+state/actions';
 import { selectFilteredFlights } from './+state/selectors';
 import { addMinutes } from '@demo/shared/util-common';
 
-// function equal(a:unknown, b:unknown) {
-//     return a === b;
-// }
-
-// -- 4 --
 @Injectable({ providedIn: 'root' })
 export class FlightBookingFacade {
   private store = inject(Store);
@@ -27,59 +22,3 @@ export class FlightBookingFacade {
     this.store.dispatch(ticketingActions.updateFlight({ flight }));
   }
 }
-
-// -- 3 --
-// @Injectable({providedIn: 'root'})
-// export class FlightBookingFacade {
-
-//     private store = inject(Store);
-//     private flightService = inject(FlightService);
-
-//     readonly flights = this.store.selectSignal(selectFilteredFlights);
-
-//     async load(from: string, to: string): Promise<void> {
-//         const flights = await this.flightService.findPromise(from, to);
-//         this.store.dispatch(ticketingActions.flightsLoaded({ flights }));
-//     }
-
-// }
-
-// -- 2 --
-// @Injectable({providedIn: 'root'})
-// export class FlightBookingFacade {
-
-//     private store = inject(Store);
-//     private flightService = inject(FlightService);
-
-//     readonly flights = this.store.selectSignal(ticketingFeature.selectFlights);
-
-//     async load(from: string, to: string): Promise<void> {
-//         const flights = await this.flightService.findPromise(from, to);
-//         this.store.dispatch(ticketingActions.flightsLoaded({ flights }));
-//     }
-
-// }
-
-// -- 1 --
-// @Injectable({providedIn: 'root'})
-// export class FlightBookingFacade {
-
-//     private flightService = inject(FlightService);
-
-//     private state = signal({
-//         flights: [] as Flight[],
-//         from: 'Hamburg',
-//         to: 'Graz'
-//     });
-
-//     readonly flights = computed(() => this.state().flights, { equal });
-
-//     async load(from: string, to: string): Promise<void> {
-//         const flights = await this.flightService.findPromise(from, to);
-//         this.state.update(state => ({
-//             ...state,
-//             flights
-//         }))
-//     }
-
-// }
