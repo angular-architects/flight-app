b43eae348f5e5255399b96b0b1bb0cc3ef514471
mikezks <23002661+mikezks@users.noreply.github.com>
2023-06-24 12:45:07 +0200

    feat: initial local state component store setup

--- src/app/domains/ticketing/ui-common/flight-filter/flight-filter.component.ts
+++ src/app/domains/ticketing/ui-common/flight-filter/flight-filter.component.ts
@@ -10,6 +10,15 @@ import {
 } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FlightFilter } from '@demo/ticketing/data';
+import { ComponentStore } from '@ngrx/component-store';
+
+export interface LocalState {
+  filters: FlightFilter[];
+}
+
+export const initialLocalState: LocalState = {
+  filters: [],
+};
 
 @Component({
   selector: 'app-flight-filter',
@@ -17,9 +26,11 @@ import { FlightFilter } from '@demo/ticketing/data';
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './flight-filter.component.html',
   styleUrls: ['./flight-filter.component.css'],
+  providers: [ComponentStore],
 })
 export class FlightFilterComponent {
   private fb = inject(FormBuilder);
+  private localStore = inject(ComponentStore<LocalState>);
   #filter = signal<FlightFilter>({ from: '', to: '', urgent: false });
   @Input() set filter(filter: FlightFilter) {
     this.#filter.set(filter);
@@ -34,6 +45,7 @@ export class FlightFilterComponent {
 
   constructor() {
     effect(() => this.filterForm.setValue(this.#filter()));
+    this.localStore.setState(initialLocalState);
   }
 
   search(): void {
