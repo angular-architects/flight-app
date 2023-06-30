903ce3337519071e4d93071fb8afcb54e10d4d54
Manfred Steyer <manfred.steyer@gmx.net>
2023-06-30 17:09:48 +0200

    add flight typeahead

---
+++ src/app/domains/ticketing/feature-booking/flight-lookup/flight-lookup.component.ts
@@ -0,0 +1,46 @@
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlightLookupFacade } from '@demo/ticketing/data';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, filter, Subject, takeUntil } from 'rxjs';
+
+@Component({
+  selector: 'app-flight-lookup',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './flight-lookup.component.html',
+  styleUrls: ['./flight-lookup.component.css'],
+})
+export class FlightLookupComponent implements OnInit, OnDestroy {
+  facade = inject(FlightLookupFacade);
+
+  control = new FormControl('', { nonNullable: true });
+
+  // Source
+  private close$ = new Subject<void>();
+
+  from$ = this.control.valueChanges.pipe(
+    filter((v) => v.length >= 3),
+    debounceTime(300)
+  );
+
+  // Sink
+  flights$ = this.facade.flights$;
+  error$ = this.facade.error$;
+  loading$ = this.facade.loading$;
+  online$ = this.facade.online$;
+
+  ngOnInit(): void {
+    this.from$.subscribe((value) => {
+      this.facade.lookup(value);
+    });
+
+    this.online$.pipe(takeUntil(this.close$)).subscribe((v) => {
+      console.log('online', v);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.close$.next();
+  }
+}
