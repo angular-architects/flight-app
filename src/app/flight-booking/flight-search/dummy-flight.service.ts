e7f7b56deec882fa6fc13d212c0761958ea172c6
Manfred Steyer <manfred.steyer@gmx.net>
2023-09-13 22:34:16 +0200

    update to angular 16

--- src/app/flight-booking/flight-search/dummy-flight.service.ts
+++ src/app/flight-booking/flight-search/dummy-flight.service.ts
@@ -8,10 +8,29 @@ export class DummyFlightService implements FlightService {
   find(from: string, to: string): Observable<Flight[]> {
     const date = new Date().toISOString();
 
-    return of([
-      { id: 7, from: 'here', to: 'there', date, delayed: false },
-      { id: 8, from: 'here', to: 'there', date, delayed: false },
-      { id: 9, from: 'here', to: 'there', date, delayed: false },
-    ]);
+    const flights = [
+      { id: 7, from: 'Graz', to: 'Hamburg', date, delayed: false },
+      { id: 8, from: 'Graz', to: 'Hamburg', date, delayed: false },
+      { id: 9, from: 'Hamburg', to: 'Graz', date, delayed: false },
+      { id: 10, from: 'Hamburg', to: 'Graz', date, delayed: false },
+      { id: 11, from: 'London', to: 'Paris', date, delayed: false },
+      { id: 12, from: 'London', to: 'Paris', date, delayed: false },
+      { id: 13, from: 'Paris', to: 'London', date, delayed: false },
+      { id: 14, from: 'Paris', to: 'London', date, delayed: false },
+      { id: 15, from: 'Bern', to: 'London', date, delayed: false },
+      { id: 16, from: 'Bern', to: 'London', date, delayed: false },
+      { id: 17, from: 'Berlin', to: 'London', date, delayed: false },
+      { id: 18, from: 'Berlin', to: 'London', date, delayed: false },
+    ];
+
+    const result = flights.filter(
+      (f) => f.from.startsWith(from) && f.to.startsWith(to)
+    );
+
+    return of(result);
+  }
+
+  findById(id: string): Observable<Flight> {
+    throw new Error('Method not implemented.');
   }
 }
