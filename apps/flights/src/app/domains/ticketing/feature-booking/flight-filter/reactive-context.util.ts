c05e0c4fd1400d73ca1499d43046bd4372671964
mikezks <23002661+mikezks@users.noreply.github.com>
2023-10-25 18:23:08 +0200

    feat: reactive context util

---
+++ apps/flights/src/app/domains/ticketing/feature-booking/flight-filter/reactive-context.util.ts
@@ -0,0 +1,10 @@
+import { Signal, EffectRef, effect, untracked } from '@angular/core';
+
+export const triggerNonReactiveContext = <T>(
+  triggerSignal: Signal<T>,
+  logic: (trigger: T) => void
+): EffectRef =>
+  effect(() => {
+    const value = triggerSignal();
+    untracked(() => logic(value));
+  });
