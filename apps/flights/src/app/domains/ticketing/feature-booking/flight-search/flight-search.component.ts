b405e52087ef3c3d3223d8722be4f01ae80fe318
mikezks <23002661+mikezks@users.noreply.github.com>
2023-10-25 17:10:04 +0200

    feat: flight search w/ filter

--- apps/flights/src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
+++ apps/flights/src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
@@ -1,50 +1,50 @@
+import { AsyncPipe, JsonPipe, NgForOf, NgIf } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { CityPipe } from '@demo/shared/ui-common';
-import { Flight, FlightService } from '@demo/ticketing/data';
+import { Flight, FlightFilter, FlightService } from '../../data';
 import { FlightCardComponent } from '../flight-card/flight-card.component';
-
-// import { CheckinService } from '@demo/checkin/data';
+import { FlightFilterComponent } from '../flight-filter/flight-filter.component';
 
 @Component({
-  selector: 'app-flight-search',
   standalone: true,
+  imports: [
+    // CommonModule,
+    NgIf,
+    NgForOf,
+    AsyncPipe,
+    JsonPipe,
+
+    FormsModule,
+    FlightCardComponent,
+    FlightFilterComponent,
+  ],
+  selector: 'app-flight-search',
   templateUrl: './flight-search.component.html',
-  styleUrls: ['./flight-search.component.css'],
-  imports: [CommonModule, FormsModule, CityPipe, FlightCardComponent],
 })
 export class FlightSearchComponent {
-  from = 'Paris';
-  to = 'London';
-  flights: Array<Flight> = [];
-  selectedFlight: Flight | undefined;
-  message = '';
-  date = new Date();
+  private flightService = inject(FlightService);
+
+  filter = {
+    from: 'Hamburg',
+    to: 'Graz',
+    urgent: false,
+  };
+  flights: Flight[] = [];
 
   basket: Record<number, boolean> = {
     3: true,
     5: true,
   };
 
-  private flightService = inject(FlightService);
+  search(filter: FlightFilter): void {
+    this.filter = filter;
 
-  search(): void {
-    // Reset properties
-    this.message = '';
-    this.selectedFlight = undefined;
+    if (!this.filter.from || !this.filter.to) return;
 
-    this.flightService.find(this.from, this.to).subscribe({
-      next: (flights) => {
+    this.flightService
+      .find(this.filter.from, this.filter.to, this.filter.urgent)
+      .subscribe((flights) => {
         this.flights = flights;
-      },
-      error: (errResp) => {
-        console.error('Error loading flights', errResp);
-      },
-    });
-  }
-
-  select(f: Flight): void {
-    this.selectedFlight = { ...f };
+      });
   }
 }
