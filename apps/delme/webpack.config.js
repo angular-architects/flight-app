a45579aa21a7821ba8a3349a9b536a6616344f9c
Manfred Steyer <manfred.steyer@gmx.net>
2023-10-26 22:38:41 +0200

    WIP

---
+++ apps/delme/webpack.config.js
@@ -0,0 +1,20 @@
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+  name: 'delme',
+
+  exposes: {
+    './Component': './apps/delme/src/app/app.component.ts',
+  },
+
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    }),
+  },
+});
