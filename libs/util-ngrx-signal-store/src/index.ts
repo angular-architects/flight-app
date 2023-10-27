6d059988564e1bd44ec7680ac632b050d38c44f1
mikezks <23002661+mikezks@users.noreply.github.com>
2023-10-27 13:42:28 +0200

    fix: ngrx signals date

--- libs/util-ngrx-signal-store/src/index.ts
+++ libs/util-ngrx-signal-store/src/index.ts
@@ -1,7 +1,7 @@
 /**
  * Package: NgRx Signals by Marko Stanimirovic & the NgRx Team
  * Version: https://github.com/ngrx/platform/tree/main/modules/signals
- * Date:    25 Aug 2023
+ * Date:    25 Oct 2023
  */
 
 export * from './src/index';
