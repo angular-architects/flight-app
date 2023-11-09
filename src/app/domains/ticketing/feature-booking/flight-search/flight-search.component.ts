1a7a1a6fb4f584870beaa0874eb6771e19a68f4b
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-11-09 23:09:04 +0100

    lab502-signals-solution

--- src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
+++ src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
@@ -1,9 +1,11 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { FlightCardComponent } from '../flight-card/flight-card.component';
-import { CityPipe } from '@demo/shared/ui-common';
-import { Flight, FlightService } from '@demo/ticketing/data';
+import {Component, computed, effect, inject, signal} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {FlightCardComponent} from '../flight-card/flight-card.component';
+import {CityPipe} from '@demo/shared/ui-common';
+import {Flight, FlightService} from '@demo/ticketing/data';
+import {toObservable, toSignal} from "@angular/core/rxjs-interop";
+import {debounceTime} from "rxjs";
 
 // import { CheckinService } from '@demo/checkin/data';
 
@@ -15,17 +17,33 @@ import { Flight, FlightService } from '@demo/ticketing/data';
   imports: [CommonModule, FormsModule, CityPipe, FlightCardComponent],
 })
 export class FlightSearchComponent {
-  from = 'Paris';
-  to = 'London';
-  flights: Array<Flight> = [];
+  from = signal('Paris');
+  to = signal('London');
+  flights = signal<Array<Flight>>([]);
   selectedFlight: Flight | undefined;
   message = '';
   date = new Date();
 
-  basket: Record<number, boolean> = {
+  basket = signal<Record<number, boolean>>({
     3: true,
     5: true,
-  };
+  })
+
+  constructor() {
+    effect(
+      () => console.log(this.flightRoute())
+    );
+  }
+
+  lazyFrom$ = toObservable(this.from).pipe(
+    debounceTime(300)
+  );
+
+  lazyFrom = toSignal(this.lazyFrom$, {
+    initialValue: this.from()
+  });
+
+  flightRoute = computed(() => 'From ' + this.lazyFrom() + ' to ' + this.to() + '.');
 
   private flightService = inject(FlightService);
 
@@ -34,9 +52,9 @@ export class FlightSearchComponent {
     this.message = '';
     this.selectedFlight = undefined;
 
-    this.flightService.find(this.from, this.to).subscribe({
+    this.flightService.find(this.from(), this.to()).subscribe({
       next: (flights) => {
-        this.flights = flights;
+        this.flights.set(flights);
       },
       error: (errResp) => {
         console.error('Error loading flights', errResp);
@@ -45,6 +63,10 @@ export class FlightSearchComponent {
   }
 
   select(f: Flight): void {
-    this.selectedFlight = { ...f };
+    this.selectedFlight = {...f};
+  }
+
+  updateBasket(id: number, selected: boolean): void {
+    this.basket.update(basket => ({...basket, [id]: selected}));
   }
 }
