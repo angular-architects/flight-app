510034c08b0d0b1740a3c2fc7898d0f736b460b5
Manfred Steyer <manfred.steyer@gmx.net>
2023-11-10 20:03:51 +0100

    add normalization

--- apps/flights/src/app/domains/ticketing/feature-booking/booking.store.ts
+++ apps/flights/src/app/domains/ticketing/feature-booking/booking.store.ts
@@ -7,18 +7,47 @@ import {
   withMethods,
   withState,
 } from '@ngrx/signals';
-import { Flight, FlightService, initFlight } from '../data';
-import { computed, inject } from '@angular/core';
+import { Flight, FlightService } from '../data';
+import { computed, effect, inject } from '@angular/core';
 import { addMinutes } from 'date-fns';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
-import { withEntities, setAllEntities } from '@ngrx/signals/entities';
-
+import {
+  withEntities,
+  setAllEntities,
+  setEntities,
+} from '@ngrx/signals/entities';
 import { debounceTime, filter, switchMap, tap } from 'rxjs';
 
 export type Criteria = {
   from: string;
   to: string;
 };
+
+export type FlightState = Flight & {
+  passengerIds: number[];
+};
+
+export type Passenger = {
+  id: number;
+  firstName: string;
+  lastName: string;
+};
+
+export type PassengerState = Passenger & {
+  flightIds: number[];
+};
+
+export type FlightsWithPassengers = Flight & {
+  passengers: Passenger[];
+};
+
+function toFlightState(flight: Flight): FlightState {
+  return { ...flight, passengerIds: [1, 2, 3] };
+}
+
+function toFlightStateArray(flights: Flight[]): FlightState[] {
+  return flights.map((f) => toFlightState(f));
+}
 
 export const BookingStore = signalStore(
   { providedIn: 'root' },
@@ -27,12 +56,19 @@ export const BookingStore = signalStore(
     to: 'Hamburg',
     basket: {} as Record<number, boolean>,
   }),
-  withEntities({ entity: type<Flight>(), collection: 'flight' }),
-  withComputed(({ basket, from, to, flightEntities }) => ({
+  withEntities({ entity: type<FlightState>(), collection: 'flight' }),
+  withEntities({ entity: type<PassengerState>(), collection: 'passenger' }),
+  withComputed(({ basket, from, to, flightEntities, passengerEntityMap }) => ({
     selectedFlights: computed(() =>
       flightEntities().filter((f) => basket()[f.id])
     ),
     criteria: computed(() => ({ from: from(), to: to() })),
+    flightsWithPassengers: computed<FlightsWithPassengers[]>(() =>
+      flightEntities().map((f) => ({
+        ...f,
+        passengers: f.passengerIds.map((p) => passengerEntityMap()[p]),
+      }))
+    ),
   })),
   withMethods((state) => {
     const flightService = inject(FlightService);
@@ -58,7 +94,12 @@ export const BookingStore = signalStore(
         const newFlight: Flight = { ...oldFlight, date: newDate.toISOString() };
         const newFlights = [newFlight, ...oldFlights.slice(1)];
 
-        patchState(state, setAllEntities(newFlights, { collection: 'flight' }));
+        patchState(
+          state,
+          setAllEntities(toFlightStateArray(newFlights), {
+            collection: 'flight',
+          })
+        );
       },
       async load(): Promise<void> {
         if (!state.from() || !state.to()) {
@@ -69,7 +110,11 @@ export const BookingStore = signalStore(
           state.from(),
           state.to()
         );
-        patchState(state, setAllEntities(flights, { collection: 'flight' }));
+
+        patchState(
+          state,
+          setAllEntities(toFlightStateArray(flights), { collection: 'flight' })
+        );
       },
       connectCriteria: rxMethod<Criteria>((c$) =>
         c$.pipe(
@@ -77,15 +122,51 @@ export const BookingStore = signalStore(
           debounceTime(300),
           switchMap((c) => flightService.find(c.from, c.to)),
           tap((flights) =>
-            patchState(state, setAllEntities(flights, { collection: 'flight' }))
+            patchState(
+              state,
+              setAllEntities(toFlightStateArray(flights), {
+                collection: 'flight',
+              })
+            )
           )
         )
       ),
     };
   }),
   withHooks({
-    onInit({ connectCriteria, criteria }) {
+    onInit(state) {
+      const { connectCriteria, criteria, flightsWithPassengers } = state;
       connectCriteria(criteria);
+      patchState(
+        state,
+        setEntities(
+          [
+            {
+              id: 1,
+              firstName: 'Max',
+              lastName: 'Muster',
+              flightIds: [10, 20, 30],
+            },
+            {
+              id: 2,
+              firstName: 'Susi',
+              lastName: 'Sorglos',
+              flightIds: [10, 20, 30],
+            },
+            {
+              id: 3,
+              firstName: 'Jane',
+              lastName: 'Doe',
+              flightIds: [10, 20, 30],
+            },
+          ],
+          { collection: 'passenger' }
+        )
+      );
+
+      effect(() => {
+        console.log('vm', flightsWithPassengers());
+      });
     },
     onDestroy(store) {
       console.log('destroy!', store);
