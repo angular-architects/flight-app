c1f669cc03ecd130b15202a687c11005c0017e40
Manfred Steyer <manfred.steyer@gmx.net>
2024-03-05 19:47:43 +0100

    remove snack bar

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,14 @@ import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app/app.component';
 import { NextFlightsModule } from './app/next-flights/next-flights.module';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
     importProvidersFrom(NextFlightsModule),
     importProvidersFrom(MatDialogModule),
-    importProvidersFrom(MatSnackBarModule),
-    provideAnimations(),
   ],
 });
