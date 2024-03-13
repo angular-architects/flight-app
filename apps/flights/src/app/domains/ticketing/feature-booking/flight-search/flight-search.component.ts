df85e94493a9f23ad9c84a061e042aef482467d5
Manfred Steyer <manfred.steyer@gmx.net>
2024-03-13 20:05:08 +0100

    convert back to rxjs

--- apps/flights/src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
+++ apps/flights/src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
@@ -28,16 +28,15 @@ export class FlightSearchComponent {
     5: true,
   });
 
-  async search(): Promise<void> {
-    if (!this.from() || !this.to()) {
-      return;
-    }
-
-    const flights = await this.flightService.findPromise(
-      this.from(),
-      this.to()
-    );
-    this.flights.set(flights);
+  search(): void {
+    this.flightService.find(this.from(), this.to()).subscribe({
+      next: (flights) => {
+        this.flights.set(flights);
+      },
+      error: (errResp) => {
+        console.error('Error loading flights', errResp);
+      },
+    });
   }
 
   delay(): void {
