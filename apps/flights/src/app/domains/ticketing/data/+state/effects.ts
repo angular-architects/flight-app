03ce211fc3184d7152f7c32b60296ad054f70c31
Manfred Steyer <manfred.steyer@gmx.net>
2024-03-13 21:38:15 +0100

    add error handling

--- apps/flights/src/app/domains/ticketing/data/+state/effects.ts
+++ apps/flights/src/app/domains/ticketing/data/+state/effects.ts
@@ -2,24 +2,27 @@ import { Injectable, inject } from '@angular/core';
 import { FlightService } from '../flight.service';
 import { Actions, ofType, createEffect } from '@ngrx/effects';
 import { ticketingActions } from './actions';
-import { catchError, map, of, switchMap } from 'rxjs';
+import { Observable, catchError, map, of, switchMap } from 'rxjs';
+import { Action } from '@ngrx/store';
 
 @Injectable({ providedIn: 'root' })
 export class TicketingEffects {
   flightService = inject(FlightService);
   actions$ = inject(Actions);
 
+  private find(from: string, to: string): Observable<Action> {
+    return this.flightService.find(from, to).pipe(
+      map((flights) => ticketingActions.flightsLoaded({ flights })),
+      catchError((error) =>
+        of(ticketingActions.error({ error: error.message }))
+      )
+    );
+  }
+
   loadFlight = createEffect(() =>
     this.actions$.pipe(
       ofType(ticketingActions.loadFlights),
-      switchMap((a) =>
-        this.flightService.find(a.from, a.to).pipe(
-          map((flights) => ticketingActions.flightsLoaded({ flights })),
-          catchError((error) =>
-            of(ticketingActions.error({ error: error.message }))
-          )
-        )
-      )
+      switchMap((a) => this.find(a.from, a.to))
     )
   );
 }
