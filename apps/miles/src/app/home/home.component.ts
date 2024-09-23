56d49498c65e494f4aad6ee5e51fb586d78fe51f
Manfred Steyer <manfred.steyer@gmx.net>
2024-09-23 12:10:49 +0200

    chore: update to nx 19 and angular 18

--- apps/miles/src/app/home/home.component.ts
+++ apps/miles/src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-home',
+  selector: 'flight-demo-home',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './home.component.html',
