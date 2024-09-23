f2485d7f2b64aa275cfa06938769366906d63da6
Manfred Steyer <manfred.steyer@gmx.net>
2024-09-23 16:28:41 +0200

    chore: update data service for newest ngrx toolkit version

--- apps/flights/src/app/domains/ticketing/feature-booking/booking.store.ts
+++ apps/flights/src/app/domains/ticketing/feature-booking/booking.store.ts
@@ -25,8 +25,6 @@ import {
 
 import { withDevtools } from '@angular-architects/ngrx-toolkit';
 
-import { withDevtools } from '@angular-architects/ngrx-toolkit';
-
 export type Criteria = {
   from: string;
   to: string;
