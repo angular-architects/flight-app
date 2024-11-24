5fade6fe6a13baa2da4af2e17055dceeeb85f7ad
Manfred Steyer <manfred.steyer@gmx.net>
2024-11-24 19:02:30 +0100

    migrate to ng19

--- src/app/next-flights/checkin/checkin.component.ts
+++ src/app/next-flights/checkin/checkin.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-checkin',
-  templateUrl: './checkin.component.html',
-  styleUrls: ['./checkin.component.css'],
+    selector: 'app-checkin',
+    templateUrl: './checkin.component.html',
+    styleUrls: ['./checkin.component.css'],
+    standalone: false
 })
 export class CheckinComponent {}
