648f3e83a99347619026cfe898b5172a5cb2ea36
Manfred Steyer <manfred.steyer@gmx.net>
2024-11-24 19:02:30 +0100

    migrate to ng19

--- src/app/flight-booking/passenger-search/passenger-search.component.ts
+++ src/app/flight-booking/passenger-search/passenger-search.component.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LoggerService } from 'src/app/shared/logger/logger';
 
 @Component({
-  selector: 'app-passenger-search',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './passenger-search.component.html',
-  styleUrls: ['./passenger-search.component.css'],
+    selector: 'app-passenger-search',
+    imports: [CommonModule],
+    templateUrl: './passenger-search.component.html',
+    styleUrls: ['./passenger-search.component.css']
 })
 export class PassengerSearchComponent {
   constructor(logger: LoggerService) {
