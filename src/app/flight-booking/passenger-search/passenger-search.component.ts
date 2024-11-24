cb7ac7bfeea38c1e94b8e03b77f5ae6a3fb91728
Manfred Steyer <manfred.steyer@gmx.net>
2024-11-24 19:02:30 +0100

    migrate to ng19

--- src/app/flight-booking/passenger-search/passenger-search.component.ts
+++ src/app/flight-booking/passenger-search/passenger-search.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-passenger-search',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './passenger-search.component.html',
-  styleUrls: ['./passenger-search.component.css'],
+    selector: 'app-passenger-search',
+    imports: [CommonModule],
+    templateUrl: './passenger-search.component.html',
+    styleUrls: ['./passenger-search.component.css']
 })
 export class PassengerSearchComponent {}
