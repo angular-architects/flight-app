e7c3b94dfcae169929a397c4bf6c0713c1400159
Manfred Steyer <manfred.steyer@gmx.net>
2024-11-24 19:02:30 +0100

    migrate to ng19

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-about',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css'],
+    selector: 'app-about',
+    imports: [CommonModule],
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css']
 })
 export class AboutComponent {}
