ab3b78f57c7f5496d99bdbd40934557a43a189db
Manfred Steyer <manfred.steyer@gmx.net>
2024-11-25 16:22:04 +0100

    add computed delayed flights

--- src/app/flight-booking/flight-search/flight-search.component.ts
+++ src/app/flight-booking/flight-search/flight-search.component.ts
@@ -1,46 +1,121 @@
-import { Component, inject } from '@angular/core';
+import {
+  Component,
+  computed,
+  effect,
+  inject,
+  Injector,
+  linkedSignal,
+  signal,
+  untracked,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Flight } from '../../model/flight';
 import { FormsModule } from '@angular/forms';
 import { FlightService } from './flight.service';
 import { FlightCardComponent } from '../flight-card/flight-card.component';
 
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
-    selector: 'app-flight-search',
-    templateUrl: './flight-search.component.html',
-    styleUrls: ['./flight-search.component.css'],
-    imports: [CommonModule, FormsModule, FlightCardComponent]
+  selector: 'app-flight-search',
+  templateUrl: './flight-search.component.html',
+  styleUrls: ['./flight-search.component.css'],
+  imports: [CommonModule, FormsModule, FlightCardComponent],
 })
 export class FlightSearchComponent {
-  from = 'London';
-  to = 'Paris';
-  selectedFlight: Flight | undefined;
-  message = '';
-
-  basket: Record<number, boolean> = {
-    3: true,
-    5: true,
-  };
-
   private flightService = inject(FlightService);
 
-  get flights(): Flight[] {
-    return this.flightService.flights;
+  flights = signal<Flight[]>([]);
+  delayInMinutes = signal(0);
+
+  delayedFlights = computed(() =>
+    calcDelayed(this.flights(), this.delayInMinutes())
+  );
+
+  from = signal('London');
+  to = signal('Paris');
+  selectedFlight = signal<Flight | undefined>(undefined);
+  message = signal('');
+
+  flightRoute = computed(() => this.from() + ' to ' + this.to());
+
+  basket = signal<Record<number, boolean>>({
+    3: true,
+    5: true,
+  });
+
+  injector = inject(Injector);
+
+  snackBar = inject(MatSnackBar);
+
+  constructor() {
+    effect(() => {
+      // auto-tracking
+      this.logCriteria();
+    });
+
+    // TODO
+    // effect(() => {
+    //   const len = this.flights().length;
+    //   if (len > 0) {
+    //     this.snackBar.open(len + ' flights loaded!');
+    //   }
+    // });
+
+    // effect(() => {
+    //   this.flightService.load(this.from(), this.to());
+    //   // loading()
+    //   // userId()
+    // })
+  }
+
+  private logCriteria() {
+    console.log('from', this.from());
+    console.log('to', this.to());
   }
 
   search(): void {
     // Reset properties
-    this.message = '';
-    this.selectedFlight = undefined;
+    this.message.set('');
+    this.selectedFlight.set(undefined);
 
-    this.flightService.load(this.from, this.to);
+    this.flightService.find(this.from(), this.to()).subscribe((flights) => {
+      this.flights.set(flights);
+    });
   }
 
   select(f: Flight): void {
-    this.selectedFlight = { ...f };
+    this.selectedFlight.set(f);
+  }
+
+  updateBasket(fid: number, selected: boolean): void {
+    // const basket = this.basket();
+    // const newBasket = {...basket, [fid]: selected};
+    // this.basket.set(newBasket);
+    this.basket.update((basket) => ({
+      ...basket,
+      [fid]: selected,
+    }));
   }
 
   delay(): void {
-    this.flightService.delay();
+    this.delayInMinutes.update((d) => d + 15);
   }
+}
+
+function calcDelayed(flights: Flight[], delay: number): Flight[] {
+  const ONE_MINUTE = 1000 * 60;
+
+  if (flights.length <= 0) {
+    return [];
+  }
+
+  const oldFlights = flights;
+  const oldFlight = oldFlights[0];
+  const oldDate = new Date(oldFlight.date);
+
+  const newDate = new Date(oldDate.getTime() + delay * ONE_MINUTE);
+  const newFlight: Flight = { ...oldFlight, date: newDate.toISOString() };
+  const newFlights = [newFlight, ...oldFlights.slice(1)];
+  return newFlights;
 }
--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import {
 import { AppComponent } from './app/app.component';
 import { APP_ROUTES } from './app/app.routes';
 import { NextFlightsModule } from './app/next-flights/next-flights.module';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -17,5 +18,6 @@ bootstrapApplication(AppComponent, {
     provideRouter(APP_ROUTES, withPreloading(PreloadAllModules)),
     importProvidersFrom(NextFlightsModule),
     importProvidersFrom(MatDialogModule),
+    importProvidersFrom(MatSnackBarModule),
   ],
 });
