19bf033978468ec75a8036bf4c3efa0bf1fa0280
Manfred Steyer <manfred.steyer@gmx.net>
2024-11-27 20:39:07 +0100

    explicitly set standalone to false

--- src/app/next-flights/checkin/checkin.component.ts
+++ src/app/next-flights/checkin/checkin.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: false,
   selector: 'app-checkin',
   templateUrl: './checkin.component.html',
   styleUrls: ['./checkin.component.css'],
