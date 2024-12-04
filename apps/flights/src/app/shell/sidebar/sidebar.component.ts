5a72df58430ce11f441ee1ece84930b777f6413c
Manfred Steyer <manfred.steyer@gmx.net>
2024-12-04 17:31:34 +0100

    update to nx20+ng19

--- apps/flights/src/app/shell/sidebar/sidebar.component.ts
+++ apps/flights/src/app/shell/sidebar/sidebar.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink],
   selector: 'app-sidebar-cmp',
   templateUrl: 'sidebar.component.html',
 })
