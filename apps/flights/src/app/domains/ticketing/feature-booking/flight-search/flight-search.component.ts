7cc65c8d1c44c7435c7098f99f0b80555624bb4c
Manfred Steyer <manfred.steyer@gmx.net>
2024-12-04 17:31:34 +0100

    update to nx20+ng19

--- apps/flights/src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
+++ apps/flights/src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
@@ -2,10 +2,7 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FlightCardComponent } from '../flight-card/flight-card.component';
-import { CityPipe } from '@demo/shared/ui-common';
 import { Flight, FlightService } from '@demo/ticketing/data';
-
-// import {  } from '@demo/checkin/data';
 
 // import {  } from '@demo/checkin/data';
 
@@ -14,7 +11,7 @@ import { Flight, FlightService } from '@demo/ticketing/data';
   standalone: true,
   templateUrl: './flight-search.component.html',
   styleUrls: ['./flight-search.component.css'],
-  imports: [CommonModule, FormsModule, CityPipe, FlightCardComponent],
+  imports: [CommonModule, FormsModule, FlightCardComponent],
 })
 export class FlightSearchComponent {
   from = 'Paris';
