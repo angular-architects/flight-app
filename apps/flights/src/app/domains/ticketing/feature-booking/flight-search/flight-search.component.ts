8e788fd61fa3b9bfaf920ab68f3fe1aed2a8100a
Manfred Steyer <manfred.steyer@gmx.net>
2024-12-04 17:31:34 +0100

    update to nx20+ng19

--- apps/flights/src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
+++ apps/flights/src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
@@ -11,7 +11,6 @@ import {
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FlightCardComponent } from '../flight-card/flight-card.component';
-import { CityPipe } from '@demo/shared/ui-common';
 import { Flight, FlightService } from '@demo/ticketing/data';
 import { addMinutes } from 'date-fns';
 import { toSignal, toObservable } from '@angular/core/rxjs-interop';
@@ -24,7 +23,7 @@ import { combineLatest, debounceTime, filter, switchMap } from 'rxjs';
   standalone: true,
   templateUrl: './flight-search.component.html',
   styleUrls: ['./flight-search.component.css'],
-  imports: [CommonModule, FormsModule, CityPipe, FlightCardComponent],
+  imports: [CommonModule, FormsModule, FlightCardComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FlightSearchComponent {
