232fa86a6e40ad15a3803e6c7405762025263fd3
Manfred Steyer <manfred.steyer@gmx.net>
2024-12-09 17:04:50 +0100

    feat: add connection b/w checkin and shared for detective demo

--- apps/flights/src/app/domains/checkin/data/checkin.service.ts
+++ apps/flights/src/app/domains/checkin/data/checkin.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { AuthService } from '@demo/shared/util-auth';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CheckinService {
+  auth = inject(AuthService);
+
   checkin(ticketNumber: string): void {
-    console.log('checking in', ticketNumber);
+    console.log('checking in', ticketNumber, this.auth.userName);
   }
 }
