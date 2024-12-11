e35bf74a622012f9a9c5fe88d623b44db9df3be4
Manfred Steyer <manfred.steyer@gmx.net>
2024-12-11 15:07:52 +0100

    fix: remove preloading for nf demo

--- apps/flights/src/main.ts
+++ apps/flights/src/main.ts
@@ -2,11 +2,7 @@ import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { bootstrapApplication } from '@angular/platform-browser';
-import {
-  PreloadAllModules,
-  provideRouter,
-  withPreloading,
-} from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { APP_ROUTES } from './app/app.routes';
 import { NextFlightsModule } from './app/domains/ticketing/feature-next-flights';
@@ -15,7 +11,7 @@ import { provideLogger, withColor } from './app/domains/shared/util-logger';
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
-    provideRouter(APP_ROUTES, withPreloading(PreloadAllModules)),
+    provideRouter(APP_ROUTES),
     importProvidersFrom(NextFlightsModule),
     importProvidersFrom(MatDialogModule),
 
