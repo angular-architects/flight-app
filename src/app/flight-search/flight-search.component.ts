9827416d97b674e029d2f0fd75b2039711f8be9f
Manfred Steyer <manfred.steyer@gmx.net>
2025-02-17 15:18:33 +0100

    add resource

--- src/app/flight-search/flight-search.component.ts
+++ src/app/flight-search/flight-search.component.ts
@@ -1,4 +1,10 @@
-import { Component, inject } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  resource,
+  signal,
+} from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { Flight } from '../model/flight';
@@ -6,6 +12,10 @@ import { FormsModule } from '@angular/forms';
 import { FlightService } from './flight.service';
 import { FlightCardComponent } from '../flight-card/flight-card.component';
 import { addMinutes } from '../shared/add-minutes';
+
+import {
+  firstValueFrom,
+} from 'rxjs';
 
 @Component({
   selector: 'app-flight-search',
@@ -17,32 +27,70 @@ import { addMinutes } from '../shared/add-minutes';
 export class FlightSearchComponent {
   private flightService = inject(FlightService);
 
-  from = 'London';
-  to = 'Paris';
-  flights: Flight[] = [];
+  from = signal('London');
+  to = signal('Paris');
+  flightRoutes = computed(() => this.from() + ' - ' + this.to());
 
-  basket: Record<number, boolean> = {
+  criteria = computed(() => ({
+    from: this.from(),
+    to: this.to(),
+  }));
+
+  flightsResource = resource({
+    request: this.criteria,
+    loader: async (params) => {
+      await waitPromise(300);
+      return await
+        firstValueFrom(this.flightService.find(params.request.from, params.request.to /*, abortSignal*/));
+    }
+  });
+
+  // flightsResource = rxResource({
+  //   // Angular 19.2: defaultValue: [],
+  //   request: this.criteria,
+  //   loader: (params) => {
+  //     return of(params).pipe(
+  //       delay(300),
+  //       switchMap((p) =>
+  //         this.flightService.find(p.request.from, p.request.to),
+  //       ),
+  //     );
+  //   },
+  // });
+
+  delayInMinutes = signal(0);
+
+  flights = computed(() => this.flightsResource.value() ?? []);
+  flightsWithDelay = computed(() =>
+    this.toFlightsWithDelay(this.flights(), this.delayInMinutes()),
+  );
+
+  basket = signal<Record<number, boolean>>({
     3: true,
     5: true,
-  };
+  });
 
   updateBasket(fid: number, selected: boolean) {
-    this.basket[fid] = selected;
+    this.basket.update((basket) => ({
+      ...basket,
+      [fid]: selected,
+    }));
   }
 
   search(): void {
-    this.flightService.find(this.from, this.to).subscribe({
-      next: (flights) => {
-        this.flights = flights;
-      },
-      error: (errResp) => {
-        console.error(errResp);
-      },
-    });
+    // Not used anymore!
+    // this.flightService.find(this.from(), this.to()).subscribe({
+    //   next: (flights) => {
+    //     this.flights.set(flights);
+    //   },
+    //   error: (errResp) => {
+    //     console.error(errResp);
+    //   },
+    // });
   }
 
   delay() {
-    this.flights = this.toFlightsWithDelay(this.flights, 15);
+    this.delayInMinutes.update((delay) => delay + 15);
   }
 
   toFlightsWithDelay(flights: Flight[], delay: number): Flight[] {
@@ -55,4 +103,8 @@ export class FlightSearchComponent {
       ...flights.slice(1),
     ];
   }
+}
+
+async function waitPromise(time: number) {
+  await new Promise((resolve) => setTimeout(resolve, time));
 }
