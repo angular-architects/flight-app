b13e7e756d6ff8bc97af97a3abe64f9f34cbe780
Manfred Steyer <manfred.steyer@gmx.net>
2025-08-19 19:13:25 +0200

    chore(ent): update to ngrx 20

---
+++ src/app/next-flights/checkin/checkin.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: false,
+  selector: 'app-checkin',
+  templateUrl: './checkin.component.html',
+  styleUrls: ['./checkin.component.css'],
+})
+export class CheckinComponent {}
