a69b63492702deb806ddac7527f31596c42aad27
Manfred Steyer <manfred.steyer@gmx.net>
2025-09-27 14:59:24 +0200

    chore: update migration script

--- update.sh
+++ update.sh
@@ -13,24 +13,18 @@ while read -r BRANCH; do
   echo "===> Wechsel zu $BRANCH"
   git checkout "$BRANCH"
 
-  echo "===> Cherry-Pick von $FIXED_COMMIT"
+  # echo "===> Cherry-Pick von $FIXED_COMMIT"
   # git cherry-pick "$FIXED_COMMIT" -X theirs || (git add . && git cherry-pick --continue)
+  echo "Copying files..."
 
-  # cp /tmp/package.json package.json
+  cp /tmp/package.json package.json
+  cp /tmp/date-cva.directive.ts apps/flights/src/app/domains/shared/ui-common/date/date-cva.directive.ts 
+  
   #cp /tmp/pre-commit .husky/pre-commit 
   # git add .
-git checkout lab08a-testing -- angular.json
-
-git checkout lab08a-testing -- src/app/next-flights/next-flights.component.ts
-git checkout lab08a-testing -- src/app/next-flights/checkin/checkin.component.ts
-
-git checkout lab08a-testing -- src/styles.css
-
-git checkout lab08a-testing -- src/app/navbar/navbar.component.html
-git checkout lab08a-testing -- src/app/sidebar/sidebar.component.html
 
   git add .
-  git commit -m "chore(ent): update to ngrx 20" || echo Weiter gehts ...
+  git commit -m "chore(ent): update to ng 21 next.5 to showcase signal forms" || echo Weiter gehts ...
 
 
 done < "$BRANCH_LIST"
