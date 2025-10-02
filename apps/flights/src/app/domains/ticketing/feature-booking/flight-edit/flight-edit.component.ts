8fb4bf7ab27b5dfa1f279215802242230bb22c0f
Manfred Steyer <manfred.steyer@gmx.net>
2025-10-02 15:50:39 +0200

    done

--- apps/flights/src/app/domains/ticketing/feature-booking/flight-edit/flight-edit.component.ts
+++ apps/flights/src/app/domains/ticketing/feature-booking/flight-edit/flight-edit.component.ts
@@ -5,7 +5,128 @@ import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { DateCvaDirective } from '@demo/shared/ui-common';
 import { FlightDetailStore } from '../flight-detail.store';
-import { Control, form, required, submit } from '@angular/forms/signals';
+import {
+  apply,
+  Control,
+  customError,
+  disabled,
+  Field,
+  FieldPath,
+  form,
+  hidden,
+  readonly,
+  required,
+  schema,
+  submit,
+  validate,
+  validateAsync,
+  validateHttp,
+  validateTree,
+} from '@angular/forms/signals';
+import { Flight } from '@demo/ticketing/data';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { delay, map, Observable, of } from 'rxjs';
+
+const flightSchema = schema<Flight>((path) => {
+  required(path.from);
+  required(path.to);
+  validateCity(path.from, ['Graz', 'Frankfurt', 'New York', 'Paris', 'London']);
+  validateCityAsync(path.to);
+  validateCityHttp(path.to);
+  validateRoundTripTree(path);
+
+  // apply(path.plane, planeSchema)
+  // applyEach(path.prices, priceSchema)
+  // applyWhen(path, (ctx) => ctx.valueOf(delayed), delayedFlight)
+
+  disabled(path.to, (ctx) => ctx.valueOf(path.delayed));
+  readonly(path.to, (ctx) => ctx.valueOf(path.delayed));
+
+  // next.2, next.5
+  hidden(path.to, (ctx) => ctx.valueOf(path.delayed));
+});
+
+const flightEditSchema = schema<Flight>((path) => {
+  apply(path, flightSchema);
+  validateForLock(path.id);
+});
+
+function validateCityAsync(schema: FieldPath<string>) {
+  validateAsync(schema, {
+    params: (ctx) => ({
+      value: ctx.value(),
+    }),
+    factory: (params) => {
+      return rxResource({
+        params,
+        stream: (p) => {
+          return rxValidateAirport(p.params.value);
+        },
+      });
+    },
+    errors: (result, _ctx) => {
+      if (!result) {
+        return {
+          kind: 'airport_not_found',
+        };
+      }
+      return null;
+    },
+  });
+}
+
+function validateCityHttp(schema: FieldPath<string>) {
+  validateHttp(schema, {
+    request: (ctx) => ({
+      url: 'https://demo.angulararchitects.io/api/flight',
+      params: {
+        from: ctx.value(),
+      },
+    }),
+    errors: (result: Flight[], _ctx) => {
+      if (result.length === 0) {
+        return {
+          kind: 'airport_not_found_http',
+        };
+      }
+      return null;
+    },
+  });
+}
+
+// Simulates a server-side validation
+function rxValidateAirport(airport: string): Observable<boolean> {
+  const allowed = ['Graz', 'Hamburg', 'Zürich', 'London', 'Paris', 'New York'];
+  return of(null).pipe(
+    delay(2000),
+    map(() => allowed.includes(airport))
+  );
+}
+
+function validateRoundTripTree(schema: FieldPath<Flight>) {
+  validateTree(schema, (ctx) => {
+    const from = ctx.field.from().value();
+    const to = ctx.field.to().value();
+
+    if (from === to) {
+      return [
+        {
+          kind: 'roundtrip_tree',
+          field: ctx.field.from,
+          from,
+          to,
+        },
+        {
+          kind: 'roundtrio_tree',
+          field: ctx.field.to,
+          from,
+          to,
+        },
+      ];
+    }
+    return null;
+  });
+}
 
 @Component({
   selector: 'app-flight-edit',
@@ -28,11 +149,7 @@ export class FlightEditComponent {
 
   // saveFlightValue = this.store.saveFlightValue
 
-  // TODO: Add some validators
-  flightForm = form(this.flight, (path) => {
-    required(path.from);
-    required(path.to);
-  });
+  flightForm = form(this.flight, flightEditSchema);
 
   constructor() {
     this.route.paramMap.subscribe((params) => {
@@ -58,4 +175,33 @@ export class FlightEditComponent {
       return null;
     });
   }
+}
+
+function validateForLock(path: FieldPath<number>) {
+  validate(path, (ctx) => {
+    if (ctx.value() <= 10) {
+      return customError({
+        kind: 'locked_flight',
+        message: 'You are not allowed to edit this flight!',
+      });
+    }
+    return null;
+  });
+}
+
+// -- put it to different files in practice --
+
+function validateCity(path: FieldPath<string>, allowedCities: string[]) {
+  validate(path, (ctx) => {
+    const from = ctx.value();
+    if (allowedCities.includes(from)) {
+      return null;
+    }
+    return customError({
+      kind: 'city_not_supported',
+      message: 'This airport is not suported',
+      actual: from,
+      allowed: allowedCities,
+    });
+  });
 }
