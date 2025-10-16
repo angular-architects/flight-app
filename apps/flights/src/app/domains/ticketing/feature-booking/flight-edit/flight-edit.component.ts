6f38c5a5711fe3c2d36325f08c5fa748712303a8
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-10-16 15:25:00 -0400

    demo: validator, custom validators, custom field property

--- apps/flights/src/app/domains/ticketing/feature-booking/flight-edit/flight-edit.component.ts
+++ apps/flights/src/app/domains/ticketing/feature-booking/flight-edit/flight-edit.component.ts
@@ -1,27 +1,64 @@
 import { httpResource } from '@angular/common/http';
-import { Component, input, numberAttribute, signal } from '@angular/core';
+import { Component, computed, input, numberAttribute } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { Control, form } from '@angular/forms/signals';
+import {
+  Control,
+  createProperty,
+  customError,
+  FieldPath,
+  form,
+  property,
+  required,
+  schema,
+  validate,
+} from '@angular/forms/signals';
 import { Flight, initFlight } from '@demo/ticketing/data';
+
+// Custom Field Property
+const ALLOWED_CITIES = createProperty<string[]>();
+
+// Custom Validator
+function validateCity(field: FieldPath<string>, cities: string[]): void {
+  // Set Custom Field Property
+  property(field, ALLOWED_CITIES, () => cities);
+  validate(field, ({ value }) =>
+    !cities.find((city) => city.startsWith(value()) || value().startsWith(city))
+      ? customError({
+          kind: 'forbiddenCity',
+          message:
+            'This city name is not allowed. Please use one of the following: ' +
+            cities.join(', '),
+        })
+      : undefined
+  );
+}
+
+// (3) Field Logic: validators, async validators, readonly, hidden, disables
+const flightSchema = schema<Flight>((flightPath) => {
+  required(flightPath.from);
+  validateCity(flightPath.from, [
+    'New York',
+    'Los Angeles',
+    'Baltimore',
+    'Paris',
+  ]);
+  required(flightPath.to);
+  validateCity(flightPath.to, ['London', 'Berlin', 'Madrid']);
+});
 
 @Component({
   selector: 'app-flight-edit',
   imports: [
     ReactiveFormsModule,
-    // (3) UI Control: Template Binding
+    // (4) UI Control: Template Binding
     Control,
   ],
   templateUrl: './flight-edit.component.html',
   styleUrls: ['./flight-edit.component.css'],
 })
 export class FlightEditComponent {
-  // (1) Data Model: Writable Signal
-  /*  flight = signal({
-    ...initFlight,
-    from: 'Rome'
-  }); */
-
   id = input(0, { transform: numberAttribute });
+
   // (1) Data Model: Writable Signal
   flightResource = httpResource<Flight>(
     () => ({
@@ -32,15 +69,11 @@ export class FlightEditComponent {
   );
 
   // (2) Field State: value, valid, touched, dirty, ...
-  editForm = form(this.flightResource.value);
-
-  constructor() {
-    /* effect(() => {
-      if (this.flightResource.hasValue()) {
-        this.editForm.patchValue(this.flightResource.value());
-      }
-    }); */
-  }
+  editForm = form(this.flightResource.value, flightSchema);
+  fromAllowedCities = computed(() =>
+    this.editForm.from().property(ALLOWED_CITIES)
+  );
+  toAllowedCities = computed(() => this.editForm.to().property(ALLOWED_CITIES));
 
   save(): void {
     console.log(this.editForm().value());
