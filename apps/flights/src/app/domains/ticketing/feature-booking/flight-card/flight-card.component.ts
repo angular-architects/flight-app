12aa049702bb5f8438ae38692242f4465da92564
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-10-16 13:29:39 -0400

    refactor: ui cmp input, model

--- apps/flights/src/app/domains/ticketing/feature-booking/flight-card/flight-card.component.ts
+++ apps/flights/src/app/domains/ticketing/feature-booking/flight-card/flight-card.component.ts
@@ -1,8 +1,15 @@
 import { DatePipe } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Output,
+  input,
+  model,
+  output,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CityPipe, StatusToggleComponent } from '@demo/shared/ui-common';
-import { initFlight } from '@demo/ticketing/data';
+import { Flight } from '@demo/ticketing/data';
 
 @Component({
   selector: 'app-flight-card',
@@ -12,12 +19,12 @@ import { initFlight } from '@demo/ticketing/data';
   styleUrls: ['./flight-card.component.css'],
 })
 export class FlightCardComponent {
-  @Input() item = initFlight;
-  @Input() selected = false;
-  @Output() selectedChange = new EventEmitter<boolean>();
+  readonly item = input.required<Flight>();
+  readonly selected = model(false);
+  itemChange = output<Flight>();
 
   toggleSelection() {
-    this.selected = !this.selected;
-    this.selectedChange.emit(this.selected);
+    this.selected.update((curr) => !curr);
+    // this.selected.set(!this.selected());
   }
 }
