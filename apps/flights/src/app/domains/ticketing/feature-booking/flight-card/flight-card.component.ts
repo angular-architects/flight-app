bd5ec62e6f799346ade85050e04a6d7e2b3fec6d
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-10-16 11:55:25 -0400

    init: flight card

--- apps/flights/src/app/domains/ticketing/feature-booking/flight-card/flight-card.component.ts
+++ apps/flights/src/app/domains/ticketing/feature-booking/flight-card/flight-card.component.ts
@@ -1,15 +1,5 @@
-import {
-  Component,
-  ElementRef,
-  EventEmitter,
-  inject,
-  Input,
-  NgZone,
-  Output,
-} from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { CommonModule } from '@angular/common';
-import { FlightEditReactiveComponent } from '../flight-edit-reactive/flight-edit-reactive.component';
+import { DatePipe } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CityPipe, StatusToggleComponent } from '@demo/shared/ui-common';
 import { initFlight } from '@demo/ticketing/data';
@@ -17,46 +7,17 @@ import { initFlight } from '@demo/ticketing/data';
 @Component({
   selector: 'app-flight-card',
   standalone: true,
-  imports: [CommonModule, CityPipe, StatusToggleComponent, RouterLink],
+  imports: [CityPipe, DatePipe, StatusToggleComponent, RouterLink],
   templateUrl: './flight-card.component.html',
   styleUrls: ['./flight-card.component.css'],
 })
 export class FlightCardComponent {
-  private element = inject(ElementRef);
-  private zone = inject(NgZone);
-
-  private dialog = inject(MatDialog);
-
   @Input() item = initFlight;
   @Input() selected = false;
   @Output() selectedChange = new EventEmitter<boolean>();
 
-  select() {
-    this.selected = true;
+  toggleSelection() {
+    this.selected = !this.selected;
     this.selectedChange.emit(this.selected);
-  }
-
-  deselect() {
-    this.selected = false;
-    this.selectedChange.emit(this.selected);
-  }
-
-  edit() {
-    this.dialog.open(FlightEditReactiveComponent, {
-      data: { flight: this.item },
-    });
-  }
-
-  blink() {
-    // Dirty Hack used to visualize the change detector
-    this.element.nativeElement.firstChild.style.backgroundColor = 'crimson';
-
-    this.zone.runOutsideAngular(() => {
-      setTimeout(() => {
-        this.element.nativeElement.firstChild.style.backgroundColor = 'white';
-      }, 1000);
-    });
-
-    return null;
   }
 }
