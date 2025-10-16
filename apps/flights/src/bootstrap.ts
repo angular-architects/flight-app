20e4a8146fbd3df5e23ecd03eff1cf73f591fcfd
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-10-16 08:42:40 -0400

    init: mfe

--- apps/flights/src/bootstrap.ts
+++ apps/flights/src/bootstrap.ts
@@ -2,20 +2,19 @@ import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { bootstrapApplication } from '@angular/platform-browser';
-import {
-  PreloadAllModules,
-  provideRouter,
-  withPreloading,
-} from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { APP_ROUTES } from './app/app.routes';
-import { NextFlightsModule } from './app/domains/ticketing/feature-next-flights';
 import { provideLogger, withColor } from './app/domains/shared/util-logger';
+import { NextFlightsModule } from './app/domains/ticketing/feature-next-flights';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
-    provideRouter(APP_ROUTES, withPreloading(PreloadAllModules)),
+    provideRouter(
+      APP_ROUTES
+      // withPreloading(PreloadAllModules)
+    ),
     importProvidersFrom(NextFlightsModule),
     importProvidersFrom(MatDialogModule),
 
--- apps/miles/src/app/app.component.ts
+++ apps/miles/src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
   selector: 'flight-demo-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
