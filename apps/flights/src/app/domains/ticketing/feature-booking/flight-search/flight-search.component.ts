d5eea3967ae04dc3789352dd1fb137a33b7e6812
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-10-31 12:21:15 +0100

    refactor: smart cmp signal, computed

--- apps/flights/src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
+++ apps/flights/src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, NgZone, inject, signal } from '@angular/core';
+import { Component, ElementRef, NgZone, computed, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FlightCardComponent } from '../flight-card/flight-card.component';
@@ -22,6 +22,9 @@ export class FlightSearchComponent {
 
   from = signal('Paris');
   to = signal('London');
+  route = computed(
+    () => 'From ' + this.from() + ' to ' + this.to() + '.'
+  );
   flights = signal<Flight[]>([]);
 
   basket = signal<Record<number, boolean>>({
