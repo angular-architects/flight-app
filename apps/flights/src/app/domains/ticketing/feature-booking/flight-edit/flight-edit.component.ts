992d8eb7018e54f1b0948cd19691af7234517664
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-10-31 14:33:43 +0100

    demo: signal forms

--- apps/flights/src/app/domains/ticketing/feature-booking/flight-edit/flight-edit.component.ts
+++ apps/flights/src/app/domains/ticketing/feature-booking/flight-edit/flight-edit.component.ts
@@ -1,40 +1,30 @@
 import { httpResource } from '@angular/common/http';
-import { Component, effect, inject, input, numberAttribute, untracked } from '@angular/core';
-import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { Flight, FlightService, initFlight } from '@demo/ticketing/data';
+import { Component, input, numberAttribute, signal } from '@angular/core';
+import { Control, form } from '@angular/forms/signals';
+import { Flight, initFlight } from '@demo/ticketing/data';
 
 @Component({
   selector: 'app-flight-edit',
-  imports: [ReactiveFormsModule],
+  imports: [
+    Control
+  ],
   templateUrl: './flight-edit.component.html',
   styleUrls: ['./flight-edit.component.css'],
 })
 export class FlightEditComponent {
-  private flightService = inject(FlightService);
-  protected editForm = inject(NonNullableFormBuilder).group({
-    id: [0],
-    from: [''],
-    to: [''],
-    date: [new Date().toISOString()],
-    delayed: [false],
-  });
+  flight = signal(initFlight);
 
+  
+  
   id = input(0, { transform: numberAttribute });
   flightResource = httpResource<Flight>(() => ({
     url: 'https://demo.angulararchitects.io/api/flight',
     params: { id: this.id() }
   }), { defaultValue: initFlight });
-
-  constructor() {
-    effect(() => untracked(() => console.log(this.id())));
-    effect(() => {
-      if (this.flightResource.hasValue()) {
-        this.editForm.patchValue(this.flightResource.value())
-      }
-    });
-  }
+  
+  editForm = form(this.flightResource.value);
 
   save(): void {
-    console.log(this.editForm.value);
+    console.log(this.editForm().value());
   }
 }
