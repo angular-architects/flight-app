31273262126ae2c617e41246134338e3ec38f821
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-10-31 14:23:51 +0100

    demo: untracked

--- apps/flights/src/app/domains/ticketing/feature-booking/flight-edit/flight-edit.component.ts
+++ apps/flights/src/app/domains/ticketing/feature-booking/flight-edit/flight-edit.component.ts
@@ -1,5 +1,5 @@
 import { httpResource } from '@angular/common/http';
-import { Component, effect, inject, input, numberAttribute } from '@angular/core';
+import { Component, effect, inject, input, numberAttribute, untracked } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { Flight, FlightService, initFlight } from '@demo/ticketing/data';
 
@@ -26,6 +26,7 @@ export class FlightEditComponent {
   }), { defaultValue: initFlight });
 
   constructor() {
+    effect(() => untracked(() => console.log(this.id())));
     effect(() => {
       if (this.flightResource.hasValue()) {
         this.editForm.patchValue(this.flightResource.value())
