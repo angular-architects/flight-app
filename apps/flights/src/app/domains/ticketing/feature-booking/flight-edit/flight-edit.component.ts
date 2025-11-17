78a2fb83b0fa9a54311aa09dff293a369b48790e
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-11-17 15:37:30 +0100

    refactor: rx to signal forms

--- apps/flights/src/app/domains/ticketing/feature-booking/flight-edit/flight-edit.component.ts
+++ apps/flights/src/app/domains/ticketing/feature-booking/flight-edit/flight-edit.component.ts
@@ -1,22 +1,25 @@
 import { httpResource } from '@angular/common/http';
-import { Component, effect, inject, input, numberAttribute } from '@angular/core';
-import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Component, input, numberAttribute, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Control, form } from '@angular/forms/signals';
 import { Flight, initFlight } from '@demo/ticketing/data';
 
 @Component({
   selector: 'app-flight-edit',
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    // (4) UI Controls: Template Bindings
+    Control
+  ],
   templateUrl: './flight-edit.component.html',
   styleUrls: ['./flight-edit.component.css'],
 })
 export class FlightEditComponent {
-  protected editForm = inject(NonNullableFormBuilder).group({
-    id: [0],
-    from: [''],
-    to: [''],
-    date: [new Date().toISOString()],
-    delayed: [false],
-  });
+  // (1) Data Model: Writable Signal
+  private flight = signal(initFlight);
+
+  // (2) Field State: valid, touched, dirty, disabled, value, ...
+  protected editForm = form(this.flight);
 
   id = input(0, { transform: numberAttribute });
   protected flightResource = httpResource<Flight>(() => ({
@@ -25,15 +28,23 @@ export class FlightEditComponent {
   }), { defaultValue: initFlight });
 
   constructor() {
-    effect(() => {
+    /* effect(() => {
       if (this.flightResource.hasValue()) {
         this.editForm.patchValue(this.flightResource.value())
       }
-    })
+    }) */
+
+    setTimeout(() => this.flight.set({
+      id: 999,
+      from: 'Madrid',
+      to: 'Rome',
+      date: new Date().toISOString(),
+      delayed: true
+    }), 5_000);
   }
 
   save(): void {
-    this.flightResource.set(this.editForm.getRawValue());
-    console.log(this.editForm.value);
+    // this.flightResource.set(this.editForm.getRawValue());
+    console.log(this.editForm().value());
   }
 }
