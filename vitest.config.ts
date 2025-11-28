0a1d30896f3522ea46d50fa43d4d5d603e4fa348
Manfred Steyer <manfred.steyer@gmx.net>
2025-11-28 10:21:16 +0100

    adjust config

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
     browser: {
       enabled: true,
       provider: playwright(),
-      ui: true,
 
       // https://vitest.dev/config/browser/playwright
       instances: [
