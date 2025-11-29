21ad899154ebb397921d72d11704f9d090c44cb0
Manfred Steyer <manfred.steyer@gmx.net>
2025-11-30 00:10:17 +0100

    chore: update to ng21

--- src/app/shared/status-toggle/status-toggle.component.ts
+++ src/app/shared/status-toggle/status-toggle.component.ts
@@ -1,11 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-status-toggle',
-    imports: [CommonModule],
-    templateUrl: './status-toggle.component.html',
-    styleUrls: ['./status-toggle.component.css']
+  selector: 'app-status-toggle',
+  imports: [],
+  templateUrl: './status-toggle.component.html',
+  styleUrls: ['./status-toggle.component.css'],
 })
 export class StatusToggleComponent {
   @Input() status = false;
