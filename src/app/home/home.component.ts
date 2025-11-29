ef0be7ae562d91110b5a39eda3509c232c09c517
Manfred Steyer <manfred.steyer@gmx.net>
2025-11-30 00:10:17 +0100

    chore: update to ng21

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-home',
-    imports: [CommonModule],
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {}
