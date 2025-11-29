04ce99796393d00eff2539942f178e1fc680db58
Manfred Steyer <manfred.steyer@gmx.net>
2025-11-30 00:10:17 +0100

    chore: update to ng21

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,27 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TabComponent } from '../shared/controls/tab/tab.component';
-import { TabbedPaneComponent } from '../shared/controls/tabbed-pane/tabbed-pane.component';
-import { ClickWithWarningDirective } from '../shared/controls/click-with-warning.directive';
-import { TooltipDirective } from '../shared/tooltip.directive';
-import { Flight } from '../model/flight';
-import { DataTableComponent } from '../shared/controls/data-table/data-table.component';
-import { TableFieldDirective } from '../shared/controls/data-table/table-field.directive';
 
 @Component({
   selector: 'app-about',
-  standalone: true,
+  imports: [],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
-  imports: [
-    CommonModule,
-    TabComponent,
-    TabbedPaneComponent,
-    ClickWithWarningDirective,
-    TooltipDirective,
-    DataTableComponent,
-    TableFieldDirective,
-  ],
 })
 export class AboutComponent {
   flights: Flight[] = [
--- src/app/flight-booking/passenger-search/passenger-search.component.ts
+++ src/app/flight-booking/passenger-search/passenger-search.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LoggerService } from 'src/app/shared/logger/logger';
 
 @Component({
-    selector: 'app-passenger-search',
-    imports: [CommonModule],
-    templateUrl: './passenger-search.component.html',
-    styleUrls: ['./passenger-search.component.css']
+  selector: 'app-passenger-search',
+  imports: [],
+  templateUrl: './passenger-search.component.html',
+  styleUrls: ['./passenger-search.component.css'],
 })
 export class PassengerSearchComponent {
   constructor(logger: LoggerService) {
