a961f93e76c0c3980af5323b97eb8d90344b6b68
Manfred Steyer <manfred.steyer@gmx.net>
2025-11-30 00:10:17 +0100

    chore: update to ng21

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { bootstrapApplication } from '@angular/platform-browser';
 import {
@@ -27,6 +27,7 @@ import { provideLogger } from './app/shared/logger/provider';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideZoneChangeDetection(),
     provideHttpClient(),
     provideRouter(APP_ROUTES, withPreloading(PreloadAllModules)),
     importProvidersFrom(NextFlightsModule),
