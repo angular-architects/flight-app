d8a86c7abae8740a15224ba06d8c96b4d7b644d7
Manfred Steyer <manfred.steyer@gmx.net>
2025-11-30 00:10:17 +0100

    chore: update to ng21

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-about',
-    imports: [CommonModule],
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.css']
+  selector: 'app-about',
+  imports: [],
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent {}
