2abd1d2861496d10db01f73d0da6964064b3e39a
Manfred Steyer <manfred.steyer@gmx.net>
2025-11-30 00:10:17 +0100

    chore: update to ng21

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-not-found',
-    imports: [CommonModule],
-    templateUrl: './not-found.component.html',
-    styleUrls: ['./not-found.component.css']
+  selector: 'app-not-found',
+  imports: [],
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.css'],
 })
 export class NotFoundComponent {}
