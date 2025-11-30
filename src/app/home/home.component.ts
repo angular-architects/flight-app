ccba6f8f79d8b8a66de44390b2cf21fc8f438c90
Manfred Steyer <manfred.steyer@gmx.net>
2025-11-30 01:40:40 +0100

    chore: provide empty about component template

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LoggerService } from '../shared/logger/logger';
 
 @Component({
   selector: 'app-home',
