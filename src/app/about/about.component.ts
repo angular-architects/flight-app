cd35d5d2d24b2df2bf7654c4800c0355ac8cfb08
Manfred Steyer <manfred.steyer@gmx.net>
2025-11-30 01:33:05 +0100

    chore: provide empty about component template

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,32 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
 })
-export class AboutComponent {
-  flights: Flight[] = [
-    {
-      id: 1,
-      from: 'Hamburg',
-      to: 'Berlin',
-      date: '2025-02-01T17:00+01:00',
-      delayed: false,
-    },
-    {
-      id: 2,
-      from: 'Hamburg',
-      to: 'Frankfurt',
-      date: '2025-02-01T17:30+01:00',
-      delayed: false,
-    },
-    {
-      id: 3,
-      from: 'Hamburg',
-      to: 'Mallorca',
-      date: '2025-02-01T17:45+01:00',
-      delayed: false,
-    },
-  ];
-
-  deleteAll(): void {
-    console.debug('delete ...');
-  }
-}
+export class AboutComponent {}
