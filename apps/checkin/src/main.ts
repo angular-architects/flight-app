b9639320f478493785af4ac4a3916bd7542057f6
Manfred Steyer <manfred.steyer@gmx.net>
2025-12-08 20:28:18 +0100

    add zone cd

--- apps/checkin/src/main.ts
+++ apps/checkin/src/main.ts
@@ -1,9 +1,8 @@
-import { HttpClientModule } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(HttpClientModule)],
+  providers: [provideHttpClient()],
 });
