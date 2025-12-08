136067964f25e68c2d1d8359fe7fcea1bcf96e5f
Manfred Steyer <manfred.steyer@gmx.net>
2025-12-08 19:56:48 +0100

    refactor: add zoneful cd for ng21

--- apps/flights/src/main.ts
+++ apps/flights/src/main.ts
@@ -1,6 +1,25 @@
-import { initFederation } from '@angular-architects/module-federation';
+import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { bootstrapApplication } from '@angular/platform-browser';
+import {
+  PreloadAllModules,
+  provideRouter,
+  withPreloading,
+} from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { APP_ROUTES } from './app/app.routes';
+import { NextFlightsModule } from '@demo/ticketing/feature-next-flights';
+import { provideLogger, withColor } from '@demo/shared/util-logger';
 
-initFederation('/assets/mf.manifest.json')
-  .catch((err) => console.error(err))
-  .then((_) => import('./bootstrap'))
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideZoneChangeDetection(),
+    provideHttpClient(),
+    provideRouter(APP_ROUTES, withPreloading(PreloadAllModules)),
+    importProvidersFrom(NextFlightsModule),
+    importProvidersFrom(MatDialogModule),
+
+    provideLogger({}, withColor()),
+  ],
+});
