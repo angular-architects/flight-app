68a67faf51198de1fe0181c283c9f21d628937ef
Manfred Steyer <manfred.steyer@gmx.net>
2025-12-08 19:56:48 +0100

    refactor: add zoneful cd for ng21

--- apps/flights/src/main.ts
+++ apps/flights/src/main.ts
@@ -1,5 +1,5 @@
 import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
@@ -10,6 +10,7 @@ import { LogLevel, provideLogger, withColor } from '@demo/shared/util-logger';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideZoneChangeDetection(),
     provideHttpClient(),
     provideRouter(APP_ROUTES),
     importProvidersFrom(NextFlightsModule),
