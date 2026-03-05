12eb6ff2385bbda694089280e93117078bacd80e
Manfred Steyer <manfred.steyer@gmx.net>
2026-03-05 15:32:55 +0100

    chore: update to ng 21.2

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [],
+  providers: [provideZoneChangeDetection()],
 });
