0765c3a2e0f1cf7bf4c3aa0879210d5ed0cc813f
Manfred Steyer <manfred.steyer@gmx.net>
2026-03-05 15:32:55 +0100

    chore: update to ng 21.2

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
-import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()],
+  providers: [provideZoneChangeDetection()],
 });
