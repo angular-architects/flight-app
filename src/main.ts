7d1d7c47b051a47f50727f384f2c005c2ea98ebd
Manfred Steyer <manfred.steyer@gmx.net>
2026-03-05 15:32:55 +0100

    chore: update to ng 21.2

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
-import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
-import { MatDialogModule } from '@angular/material/dialog';
+import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { NextFlightsModule } from './app/next-flights/next-flights.module';
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(),
-    importProvidersFrom(NextFlightsModule),
-    importProvidersFrom(MatDialogModule),
-  ],
+  providers: [provideZoneChangeDetection()],
 });
