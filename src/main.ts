3b1271ea3f5fe08b24da4f32a7f12441edef77f1
Manfred Steyer <manfred.steyer@gmx.net>
2026-03-05 15:32:55 +0100

    chore: update to ng 21.2

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,9 @@
-import { provideHttpClient } from '@angular/common/http';
+import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { DefaultFlightService } from './app/flight-search/default-flight.service';
 import { FlightService } from './app/flight-search/flight.service';
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(),
-    {
-      provide: FlightService,
-      useClass: DefaultFlightService,
-    },
-  ],
+  providers: [provideZoneChangeDetection()],
 });
